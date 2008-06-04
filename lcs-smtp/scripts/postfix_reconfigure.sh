788960ea143e2b1455de7927161ff73458749d5f
rhertzog <rhertzog@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2008-06-04 09:39:17 +0000

    Add initial release of lcs-smtp.
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/BacASable/LCS2.0ndg/sources/trunk@1169 1012e355-7652-49d3-8fd6-1f9b0b464cea

---
+++ lcs-smtp/scripts/postfix_reconfigure.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+# Make main.cf / ldap-aliases.cf 
+# GrosQuicK  Mon, 3 Dec 2007
+# BourrinM@n by jLCF <(°_-)/> on 7 Dec 2007
+#
+IN_CONFIG_PATH="/etc/lcs/postfix"
+
+if [ "$6" = "" ];
+then
+        echo "Usage: postfix_reconfigure.sh <FQDN> <DOMAIN> <LOCAL_DOMAIN> <NETWORK> <BASEDN> <RELAYHOST> <XEN_NETWORK>"
+        exit 0
+fi
+
+FQHN="$1"
+DOMAIN="$2"
+LOCAL_DOMAIN="$3"
+LAN_NETWORK="$4"
+BASE_DN="$5"
+RELAY_HOST="$6"
+XEN_NETWORK="$7"
+
+if [ -e /etc/postfix/main.cf ]; 
+then
+        mv /etc/postfix/main.cf /etc/postfix/main.cf.lcssav
+fi
+cat $IN_CONFIG_PATH/main_lcslis.cf \
+        |sed -e "s!#FQDN#!$FQHN!g" \
+        |sed -e "s!#DOMAIN#!$DOMAIN!g" \
+        |sed -e "s!#LOCAL_DOMAIN#!$LOCAL_DOMAIN!g" \
+        |sed -e "s!#LAN_NETWORK#!$LAN_NETWORK!g" \
+        |sed -e "s!#RELAY_HOST#!$RELAY_HOST!g" \
+        |sed -e "s!#XEN_NETWORK#!$XEN_NETWORK!g" \
+        >/etc/postfix/main.cf
+sed -i s/,,/,/g  /etc/postfix/main.cf
+#set the mailname
+echo "$DOMAIN" > /etc/mailname
+
+# Add alias map with ldap search filter on mail attribute
+# if not exist (must be in main_lcslis.cf sample file)
+RESULT=`cat /etc/postfix/main.cf | egrep "ldap-aliases.cf"`
+if   [ "$RESULT" = "" ] ; then
+	echo "alias_maps = hash:/etc/aliases, ldap:/etc/postfix/ldap-aliases.cf" >> /etc/postfix/main.cf
+fi
+# test if include for mailing list aliases is in place on old main.cf
+# and put it if necessary
+if [ -e /etc/postfix/main.cf.lcssav ]; then
+    RESULT=`cat /etc/postfix/main.cf.lcssav | grep "mailing_list.cf"`
+    if   [ "$RESULT" != "" ] ; then
+	echo "# Include mainling list configuration" >> /etc/postfix/main.cf
+	echo "alias_maps = ldap:/etc/postfix/mailing_list.cf" >> /etc/postfix/main.cf
+    fi
+fi
+# ldap-aliases map
+echo "server_host = localhost" > /etc/postfix/ldap-aliases.cf
+echo "search_base = $BASE_DN" >> /etc/postfix/ldap-aliases.cf
+echo "query_filter = (mail=%s@$DOMAIN)" >>  /etc/postfix/ldap-aliases.cf
+echo "result_attribute = uid" >> /etc/postfix/ldap-aliases.cf
+
+/etc/init.d/postfix restart >/dev/null
