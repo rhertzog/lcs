405c0225732820053157bdf0090875d385bdc53e
simc <simc@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2008-06-04 10:11:58 +0000

    lcs-openssh ssh + ssh pour sftp
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/BacASable/LCS2.0ndg/sources/trunk@1174 1012e355-7652-49d3-8fd6-1f9b0b464cea

---
+++ lcs-openssh-5.0p1/debian/cron.d.ex
@@ -0,0 +1,4 @@
+#
+# Regular cron jobs for the lcs-openssh package
+#
+0 4	* * *	root	lcs-openssh_maintenance
---
+++ lcs-openssh-5.0p1/debian/menu.ex
@@ -0,0 +1,2 @@
+?package(lcs-openssh):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
+  title="lcs-openssh" command="/usr/bin/lcs-openssh"
