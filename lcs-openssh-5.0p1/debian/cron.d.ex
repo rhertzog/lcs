3628a19d0984605e4b94f0d337d8f6e39cfecfc5
simc <simc@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2008-06-04 13:45:49 +0000

    nettoyage
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/BacASable/LCS2.0ndg/sources/trunk@1184 1012e355-7652-49d3-8fd6-1f9b0b464cea

--- lcs-openssh-5.0p1/debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the lcs-openssh package
-#
-0 4	* * *	root	lcs-openssh_maintenance
--- lcs-openssh-5.0p1/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(lcs-openssh):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="lcs-openssh" command="/usr/bin/lcs-openssh"
