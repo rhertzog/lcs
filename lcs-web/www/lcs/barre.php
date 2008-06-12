87a77c3cbd7097d957ee52a742533d7d963328aa
jlcf <jlcf@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2008-06-12 16:28:12 +0000

    New top menu by Yannick Chistel
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/BacASable/LCS2.0ndg/sources/trunk@1299 1012e355-7652-49d3-8fd6-1f9b0b464cea

--- lcs-web/www/lcs/barre.php
+++ lcs-web/www/lcs/barre.php
@@ -1,5 +1,5 @@
 <?php
-/* lcs/barre.php derniere mise a jour : 29/01/2008 «Barre a Mine» powered by guillaume.marquis@crdp.ac-caen.fr */
+/* lcs/barre.php derniere mise a jour : 12/06/2008 */
 require "includes/headerauth.inc.php";
 require "../Annu/includes/ldap.inc.php";
 require "../Annu/includes/ihm.inc.php";
@@ -20,12 +20,53 @@ list ($idpers, $login)= isauth();
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-  <title>...::: Barre LCS V1.4 :::...</title>
+  <title>...::: Barre LCS 2.0 :::...</title>
   <meta HTTP-EQUIV="Content-Type" CONTENT="tetx/html; charset=ISO-8859-1">
 <style type="text/css">
 body	{
 	margin-right: 6%;
 }
+#entete{
+	height:100px;
+	background: url('images/barre1/bandeau_lcs.gif');
+	background-repeat: no-repeat;
+}
+#blocleft{
+	float: left;
+	width: 100%;
+	/*border: dashed 1px green;*/
+}
+.logo{
+	float: left;
+	margin-left:10px;
+	width: 120px;
+	/*border: dashed 1px red;*/
+}
+.bouton{
+	float: left;
+	padding-top: 5px;
+	text-align: center;
+	width: 70px;
+	height: 90px;
+	/* border: dashed 1px red; */
+}
+
+.deconnect{
+	float: right;
+	padding-top: 65px;
+	text-align: center;
+	width: 70px;
+	height: 90px;
+	/* border: dashed 1px green; */
+}
+
+.connexion{
+	float: right;
+	width: 70px;
+	height: 90px;
+	padding-top: 50px;
+}
+	
 .login {
   font-size: small;
   font-weight: bold;
@@ -83,48 +124,47 @@ function MM_preloadImages() { //v3.0
 //-->
 </script>
 </head>
+
 <body bgcolor="#f8f8ff" onLoad="MM_preloadImages('images/<? echo $path ?>/BP_r1_c3_f2.gif','images/<? echo $path ?>/BP_r1_c3_f3.gif','images/<? echo $path ?>/BP_r1_c4_f2.gif','images/<? echo $path ?>/BP_r1_c4_f3.gif','images/<? echo $path ?>/BP_r1_c5_f2.gif','images/<? echo $path ?>/BP_r1_c5_f3.gif','images/<? echo $path ?>/BP_r1_c6_f2.gif','images/<? echo $path ?>/BP_r1_c6_f3.gif','images/<? echo $path ?>/BP_r1_c7_f2.gif','images/<? echo $path ?>/BP_r1_c7_f3.gif','images/<? echo $path ?>/BP_r1_c8_f2.gif','images/<? echo $path ?>/BP_r1_c8_f3.gif','images/<? echo $path ?>/BP_r1_c10_f2.gif','images/<? echo $path ?>/BP_r1_c10_f3.gif');">
-<table border="0" cellpadding="0" cellspacing="0" width="100%">
-   <tr>
-   <!-- Logo LCS -->
-   <td width="153">
-   	<a href="<? echo $url_logo ?>" target="principale">
-		<img name="BP_r1_c1" title="Documentation LCS" alt="Documentation LCS" src="images/<? echo $path ?>/BP_r1_c1.gif" width="153" height="86" border="0">   		
-	</a>
-   </td>      
-   <!-- Espace -->
-   <td><img name="BP_r1_c2" title="Espace perso" alt="Espace perso" src="images/<? echo $path ?>/BP_r1_c2.gif" width="68" height="86" border="0"></td>
-   <!-- Mon LCS -->
+<div id="entete">
+    <div id="blocleft">
+    <!-- Logo LCS -->
+        <div class="logo">
+   	    <a href="<? echo $url_logo ?>" target="principale">
+		<img name="BP_r1_c1" title="Documentation LCS" alt="Documentation LCS" src="images/<? echo $path ?>/BP_r1_c1.gif" border="0">   		
+	   </a>
+        </div>      
+        <!-- Espace -->
+        <div class="bouton"></div>
+        <!-- Mon LCS -->
 <?
 if ( $idpers==0 ) {
  	// Un utilisateur n'est pas authentifié  
-   	echo "<td><a href=\"statandgo.php?use=Accueil\" target=\"principale\" onMouseOut=\"MM_nbGroup('out');\"  onMouseOver=\"MM_nbGroup('over','BP_r1_c3','images/$path/BP_r1_c3_f2.gif','images/$path/BP_r1_c3_f3.gif',1);\"  onClick=\"MM_nbGroup('down','navbar1','BP_r1_c3','images/$path/BP_r1_c3_f3.gif',1);\" ><img name=\"BP_r1_c3\" title=\"Authentification\" alt=\"Authentification\" src=\"images/$path/BP_r1_c3.gif\" width=\"69\" height=\"86\" border=\"0\"></a></td>\n";
-   	echo "<td><a href=\"statandgo.php?use=Aide\" target=\"principale\" onMouseOut=\"MM_nbGroup('out');\"  onMouseOver=\"MM_nbGroup('over','BP_r1_c8','images/$path/BP_r1_c8_f2.gif','images/$path/BP_r1_c8_f3.gif',1);\"  onClick=\"MM_nbGroup('down','navbar1','BP_r1_c8','images/$path/BP_r1_c8_f3.gif',1);\" ><img name=\"BP_r1_c8\" title=\"Aide\" alt=\"Aide\" src=\"images/$path/BP_r1_c8.gif\" width=\"68\" height=\"86\" border=\"0\"></a></td>\n";
-        echo "<td><img name=\"BP_r1_c2\" alt=\"separateur\" src=\"images/$path/BP_r1_c2.gif\" width=\"408\" height=\"86\" border=\"0\"></td>\n";	
-	echo "<td width=\"50%\" valign=\"bottom\" align=\"right\"><img title=\"Déconnecté\" alt=\"Déconnecté\" src=\"images/deconnect.png\" width=\"26\" height=\"9\"  hspace=\"5\" align=\"bottom\" border=\"0\"></td>\n";
+   	echo "<div class=\"bouton\"><a href=\"statandgo.php?use=Accueil\" target=\"principale\" onMouseOut=\"MM_nbGroup('out');\"  onMouseOver=\"MM_nbGroup('over','BP_r1_c3','images/$path/BP_r1_c3_f2.gif','images/$path/BP_r1_c3_f3.gif',1);\"  onClick=\"MM_nbGroup('down','navbar1','BP_r1_c3','images/$path/BP_r1_c3_f3.gif',1);\" ><img name=\"BP_r1_c3\" title=\"Authentification\" alt=\"Authentification\" src=\"images/$path/BP_r1_c3.gif\"  border=\"0\"></a></div>\n";
+   	echo "<div class=\"bouton\"><a href=\"statandgo.php?use=Aide\" target=\"principale\" onMouseOut=\"MM_nbGroup('out');\"  onMouseOver=\"MM_nbGroup('over','BP_r1_c8','images/$path/BP_r1_c8_f2.gif','images/$path/BP_r1_c8_f3.gif',1);\"  onClick=\"MM_nbGroup('down','navbar1','BP_r1_c8','images/$path/BP_r1_c8_f3.gif',1);\" ><img name=\"BP_r1_c8\" title=\"Aide\" alt=\"Aide\" src=\"images/$path/BP_r1_c8.gif\"  border=\"0\"></a></div>\n";
+        echo "<div class=\"bouton\"></div>\n";	
+	echo "<div class=\"deconnect\"><img title=\"Déconnecté\" alt=\"Déconnecté\" src=\"images/deconnect.png\" width=\"26\" height=\"9\"  hspace=\"5\" align=\"bottom\" border=\"0\"></div>\n";
 	
 } else {
  	// Un utilisateur est authentifié
-	echo "<td><a href=\"statandgo.php?use=Accueil\" target=\"principale\" onMouseOut=\"MM_nbGroup('out');\"  onMouseOver=\"MM_nbGroup('over','BP_r1_c3','images/$path/mon_LCS_BP_r1_c3_f2.gif','images/$path/mon_LCS_BP_r1_c3_f3.gif',1);\"  onClick=\"MM_nbGroup('down','navbar1','BP_r1_c3','images/$path/mon_LCS_BP_r1_c3_f3.gif',1);\" ><img name=\"BP_r1_c3\" title=\"Mon LCS\" alt=\"Mon LCS\" src=\"images/$path/mon_LCS_BP_r1_c3.gif\" width=\"69\" height=\"86\" border=\"0\"></a></td>\n";
-   	echo "<td><a href=\"statandgo.php?use=Applis\" target=\"principale\"  onMouseOut=\"MM_nbGroup('out');\" onMouseOver=\"MM_nbGroup('over','BP_r1_c4','images/$path/BP_r1_c4_f2.gif','images/$path/BP_r1_c4_f3.gif',1);\"  onClick=\"MM_nbGroup('down','navbar1','BP_r1_c4','images/$path/BP_r1_c4_f3.gif',1);\" ><img name=\"BP_r1_c4\" title=\"Applications\" alt=\"Applications\" src=\"images/$path/BP_r1_c4.gif\" width=\"69\" height=\"86\" border=\"0\"></a></td>\n";
+	echo "<div class=\"bouton\"><a href=\"statandgo.php?use=Accueil\" target=\"principale\" onMouseOut=\"MM_nbGroup('out');\"  onMouseOver=\"MM_nbGroup('over','BP_r1_c3','images/$path/mon_LCS_BP_r1_c3_f2.gif','images/$path/mon_LCS_BP_r1_c3_f3.gif',1);\"  onClick=\"MM_nbGroup('down','navbar1','BP_r1_c3','images/$path/mon_LCS_BP_r1_c3_f3.gif',1);\" ><img name=\"BP_r1_c3\" title=\"Mon LCS\" alt=\"Mon LCS\" src=\"images/$path/mon_LCS_BP_r1_c3.gif\" border=\"0\"></a></div>\n";
+   	echo "<div class=\"bouton\"><a href=\"statandgo.php?use=Applis\" target=\"principale\"  onMouseOut=\"MM_nbGroup('out');\" onMouseOver=\"MM_nbGroup('over','BP_r1_c4','images/$path/BP_r1_c4_f2.gif','images/$path/BP_r1_c4_f3.gif',1);\"  onClick=\"MM_nbGroup('down','navbar1','BP_r1_c4','images/$path/BP_r1_c4_f3.gif',1);\" ><img name=\"BP_r1_c4\" title=\"Applications\" alt=\"Applications\" src=\"images/$path/BP_r1_c4.gif\" border=\"0\"></a></div>\n";
 	if ( $squirrelmail==1 ) // Webmail
-   		echo "<td><a href=\"statandgo.php?use=squirrelmail\" target=\"principale\" onMouseOut=\"MM_nbGroup('out');\" onMouseOver=\"MM_nbGroup('over','BP_r1_c5','images/$path/BP_r1_c5_f2.gif','images/$path/BP_r1_c5_f3.gif',1);\"  onClick=\"MM_nbGroup('down','navbar1','BP_r1_c5','images/$path/BP_r1_c5_f3.gif',1);\" ><img name=\"BP_r1_c5\" title=\"Webmail\" alt=\"Webmail\"  src=\"images/$path/BP_r1_c5.gif\" width=\"68\" height=\"86\" border=\"0\"></a></td>\n";
-	if ($phpweblog==1) // weblog	
-   		echo "<td><a href=\"statandgo.php?use=Forum\" target=\"principale\" onMouseOut=\"MM_nbGroup('out');\"  onMouseOver=\"MM_nbGroup('over','BP_r1_c6','images/$path/BP_r1_c6_f2.gif','images/$path/BP_r1_c6_f3.gif',1);\"  onClick=\"MM_nbGroup('down','navbar1','BP_r1_c6','images/$path/BP_r1_c6_f3.gif',1);\" ><img name=\"BP_r1_c6\" title=\"Forum\" alt=\"Forum\"  src=\"images/$path/BP_r1_c6.gif\" width=\"68\" height=\"86\" border=\"0\"></a></td>\n";
+   		echo "<div class=\"bouton\"><a href=\"statandgo.php?use=squirrelmail\" target=\"principale\" onMouseOut=\"MM_nbGroup('out');\" onMouseOver=\"MM_nbGroup('over','BP_r1_c5','images/$path/BP_r1_c5_f2.gif','images/$path/BP_r1_c5_f3.gif',1);\"  onClick=\"MM_nbGroup('down','navbar1','BP_r1_c5','images/$path/BP_r1_c5_f3.gif',1);\" ><img name=\"BP_r1_c5\" title=\"Webmail\" alt=\"Webmail\"  src=\"images/$path/BP_r1_c5.gif\" border=\"0\"></a></div>\n";
 	elseif ($spip==1) // CMS
-   		echo "<td><a href=\"statandgo.php?use=spip\" target=\"principale\" onMouseOut=\"MM_nbGroup('out');\"  onMouseOver=\"MM_nbGroup('over','BP_r1_c6','images/$path/BP_r1_c6_f2.gif','images/$path/BP_r1_c6_f3.gif',1);\"  onClick=\"MM_nbGroup('down','navbar1','BP_r1_c6','images/$path/BP_r1_c6_f3.gif',1);\" ><img name=\"BP_r1_c6\" title=\"Forum\" alt=\"Forum\" src=\"images/$path/BP_r1_c6.gif\" width=\"68\" height=\"86\" border=\"0\"></a></td>\n";	
+   		echo "<div class=\"bouton\"><a href=\"statandgo.php?use=spip\" target=\"principale\" onMouseOut=\"MM_nbGroup('out');\"  onMouseOver=\"MM_nbGroup('over','BP_r1_c6','images/$path/BP_r1_c6_f2.gif','images/$path/BP_r1_c6_f3.gif',1);\"  onClick=\"MM_nbGroup('down','navbar1','BP_r1_c6','images/$path/BP_r1_c6_f3.gif',1);\" ><img name=\"BP_r1_c6\" title=\"Forum\" alt=\"Forum\" src=\"images/$path/BP_r1_c6.gif\" border=\"0\"></a></div>\n";	
         if ( is_admin("Lcs_is_admin",$login) == "Y" ) // acces au menu d'administration	
-   	echo "<td><a href=\"statandgo.php?use=Admin\" target=\"principale\"onMouseOut=\"MM_nbGroup('out');\"  onMouseOver=\"MM_nbGroup('over','BP_r1_c7','images/$path/BP_r1_c7_f2.gif','images/$path/BP_r1_c7_f3.gif',1);\"  onClick=\"MM_nbGroup('down','navbar1','BP_r1_c7','images/$path/BP_r1_c7_f3.gif',1);\" ><img name=\"BP_r1_c7\" title=\"Administration\" alt=\"Administration\" src=\"images/$path/BP_r1_c7.gif\" width=\"69\" height=\"86\" border=\"0\"></a></td>\n";
+   	echo "<div class=\"bouton\"><a href=\"statandgo.php?use=Admin\" target=\"principale\"onMouseOut=\"MM_nbGroup('out');\"  onMouseOver=\"MM_nbGroup('over','BP_r1_c7','images/$path/BP_r1_c7_f2.gif','images/$path/BP_r1_c7_f3.gif',1);\"  onClick=\"MM_nbGroup('down','navbar1','BP_r1_c7','images/$path/BP_r1_c7_f3.gif',1);\" ><img name=\"BP_r1_c7\" title=\"Administration\" alt=\"Administration\" src=\"images/$path/BP_r1_c7.gif\" border=\"0\"></a></div>\n";
 	// Aide
-   	echo "<td><a href=\"statandgo.php?use=Aide\" target=\"principale\"  onMouseOut=\"MM_nbGroup('out');\"  onMouseOver=\"MM_nbGroup('over','BP_r1_c8','images/$path/BP_r1_c8_f2.gif','images/$path/BP_r1_c8_f3.gif',1);\"  onClick=\"MM_nbGroup('down','navbar1','BP_r1_c8','images/$path/BP_r1_c8_f3.gif',1);\" ><img name=\"BP_r1_c8\" title=\"Aide\" alt=\"Aide\" src=\"images/$path/BP_r1_c8.gif\" width=\"68\" height=\"86\" border=\"0\"></a></td>\n";
+   	echo "<div class=\"bouton\"><a href=\"statandgo.php?use=Aide\" target=\"principale\"  onMouseOut=\"MM_nbGroup('out');\"  onMouseOver=\"MM_nbGroup('over','BP_r1_c8','images/$path/BP_r1_c8_f2.gif','images/$path/BP_r1_c8_f3.gif',1);\"  onClick=\"MM_nbGroup('down','navbar1','BP_r1_c8','images/$path/BP_r1_c8_f3.gif',1);\" ><img name=\"BP_r1_c8\" title=\"Aide\" alt=\"Aide\" src=\"images/$path/BP_r1_c8.gif\" border=\"0\"></a></div>\n";
 	// Case vierge
-   	echo "<td><img name=\"BP_r1_c9\" alt=\"séparateur\"src=\"images/$path/BP_r1_c9.gif\" width=\"70\" height=\"86\" border=\"0\"></td>\n";
-   	// Deconnexion LCS
-   	echo "<td><a href=\"logout.php\" onMouseOut=\"MM_nbGroup('out');\" onMouseOver=\"MM_nbGroup('over','BP_r1_c10','images/$path/BP_r1_c10_f2.gif','images/$path/BP_r1_c10_f3.gif',1);\" onClick=\"MM_nbGroup('down','navbar1','BP_r1_c10','images/$path/BP_r1_c10_f3.gif',1);\" ><img name=\"BP_r1_c10\" title=\"Déconnexion\" alt=\"Déconnexion\" src=\"images/$path/BP_r1_c10.gif\" width=\"68\" height=\"86\" border=\"0\"></a></td>\n";
-	echo "<td width=\"50%\" valign=\"bottom\" align=\"right\"><img src=\"images/connect.png\" alt=\"Login\" width=\"26\" height=\"9\"  hspace=\"5\" align=\"bottom\" border=\"0\"><font class=\"login\">$login</font></td>\n";	
+   	echo "<div class=\"bouton\"></div>\n";
+   	// Deconnection LCS
+   	echo "<div class=\"bouton\"><a href=\"logout.php\" onMouseOut=\"MM_nbGroup('out');\" onMouseOver=\"MM_nbGroup('over','BP_r1_c10','images/$path/BP_r1_c10_f2.gif','images/$path/BP_r1_c10_f3.gif',1);\" onClick=\"MM_nbGroup('down','navbar1','BP_r1_c10','images/$path/BP_r1_c10_f3.gif',1);\" ><img name=\"BP_r1_c10\" title=\"Déconnection\" alt=\"Déconnection\" src=\"images/$path/BP_r1_c10.gif\" border=\"0\"></a></div>\n";
+	echo "<div class=\"connexion\"><img src=\"images/connect.png\" alt=\"Login\" width=\"26\" height=\"9\"  hspace=\"5\" align=\"bottom\" border=\"0\"><font class=\"login\">$login</font></div>\n";	
 }
 ?>
-  </tr>
-</table>
+    </div><!-- Fin blocleft -->
+</div><!-- Fin entete -->
 </body>
 </html>
