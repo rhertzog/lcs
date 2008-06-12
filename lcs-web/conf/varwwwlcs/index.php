21b38a7c93f345fe553300dae2a78776e82b217a
jlcf <jlcf@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2008-06-12 15:15:50 +0000

    suppression
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/BacASable/LCS2.0ndg/sources/trunk@1283 1012e355-7652-49d3-8fd6-1f9b0b464cea

--- lcs-web/conf/varwwwlcs/index.php
+++ lcs-web/conf/varwwwlcs/index.php
@@ -9,14 +9,18 @@ else
     die ("paramètres absents de la base de données");
 @mysql_free_result($result);
 
-if ( $url_redirect == "accueil.php" || $url_redirect == "../squidGuard/pageinterdite.html" )$url_accueil = $url_redirect;
+if ( $url_redirect == "../squidGuard/pageinterdite.html" ) $url_accueil = $url_redirect;
+if ( $url_redirect == "accueil.php" ) 
+    if ( is_dir ("/var/www/monlcs") )
+        $url_accueil = "/monlcs/index.php" ;
+    else $url_accueil = $url_redirect;
 ?>
 <html>
 <head>
 <title>...::: LCS :::...</title>
 <LINK REL="SHORTCUT ICON" HREF="images/favicon.ico" />
 </head>
-<FRAMESET ROWS="105,*" BORDER="0" >
+<FRAMESET ROWS="90,*" BORDER="0" >
 	<FRAME SRC="barre.php" NAME="barre" SCROLLING="no"></FRAME>
         <FRAME SRC="<? echo"$url_accueil"; ?>" NAME="principale"></FRAME>
 </FRAMESET>
