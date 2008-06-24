e6e0fda7e4accb3c3cc00dfd1ad80dfc9f0f1f44
jlcf <jlcf@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2008-06-24 13:50:27 +0000

    Add a freemind documentation for lcs web
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/BacASable/LCS2.0ndg/sources/trunk@1593 1012e355-7652-49d3-8fd6-1f9b0b464cea

---
+++ lcs-web/doc/web/html/lcsweb.mm
@@ -0,0 +1,57 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node BACKGROUND_COLOR="#e08d11" COLOR="#24244b" CREATED="1214236759289" ID="Freemind_Link_1099045357" MODIFIED="1214244881826" STYLE="bubble" TEXT="LCS WEB">
+<edge COLOR="#040452" WIDTH="2"/>
+<font BOLD="true" ITALIC="true" NAME="SansSerif" SIZE="15"/>
+<icon BUILTIN="gohome"/>
+<hook NAME="accessories/plugins/NodeNote.properties">
+<text>Paquet logiciel lcs-web&#xa;</text>
+</hook>
+<node CREATED="1214237206693" FOLDED="true" ID="_" MODIFIED="1214312146657" POSITION="right" TEXT="Modules applicatifs" VSHIFT="3">
+<cloud/>
+<hook NAME="accessories/plugins/NodeNote.properties">
+<text>Les modules applicatifs LCS sont des paquest logiciels au format Debian qui s&apos;installent via l&apos;interface web d&apos;administration ou via la commande aptitude install </text>
+</hook>
+<node CREATED="1214238324867" ID="Freemind_Link_1231372754" LINK="http://linux.crdp.ac-caen.fr/modulesLcsEtchDev/Docs/monlcs_html/" MODIFIED="1214293670103" TEXT="Mon LCS" VSHIFT="12"/>
+<node CREATED="1214238393416" ID="Freemind_Link_1156867790" LINK="http://linux.crdp.ac-caen.fr/modulesLcsEtchDev/Docs/clientftp_html/" MODIFIED="1214293671455" TEXT="ClientFtp" VSHIFT="6">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1214238434973" ID="Freemind_Link_237143666" LINK="http://linux.crdp.ac-caen.fr/modulesLcsEtchDev/Docs/spip_html/" MODIFIED="1214293672711" TEXT="CMS spip" VSHIFT="-4"/>
+<node CREATED="1214238452899" ID="Freemind_Link_292346844" LINK="http://linux.crdp.ac-caen.fr/modulesLcsEtchDev/Docs/smbwebclient_html/" MODIFIED="1214293660521" TEXT="Smbwebclient " VSHIFT="7"/>
+<node CREATED="1214238538059" ID="Freemind_Link_1633506246" LINK="http://linux.crdp.ac-caen.fr/modulesLcsEtchDev/Docs/pma_html/" MODIFIED="1214293657721" TEXT="Gestion base de donn&#xe9;es MySQL" VSHIFT="-3"/>
+</node>
+<node BACKGROUND_COLOR="#3e9d3e" CREATED="1214237243085" ID="Freemind_Link_1350358395" MODIFIED="1214293701448" POSITION="left" TEXT="Briques de base" VSHIFT="-81">
+<cloud/>
+<icon BUILTIN="xmag"/>
+<node CREATED="1214237363911" HGAP="25" ID="Freemind_Link_314630974" MODIFIED="1214247871707" TEXT="Systeme d&apos;authentification" VSHIFT="-13">
+<icon BUILTIN="password"/>
+</node>
+<node CREATED="1214237400980" HGAP="26" ID="Freemind_Link_17978470" MODIFIED="1214257119307" TEXT="Annuaire des utilisateurs" VSHIFT="-12">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="ksmiletris"/>
+</node>
+<node CREATED="1214237450035" HGAP="33" ID="Freemind_Link_415768838" MODIFIED="1214247877510" TEXT="Service de courrier" VSHIFT="1">
+<icon BUILTIN="korn"/>
+</node>
+<node CREATED="1214237505208" ID="Freemind_Link_261930423" MODIFIED="1214247873850" TEXT="Interface web d&apos;administration" VSHIFT="10">
+<icon BUILTIN="wizard"/>
+</node>
+</node>
+<node CREATED="1214237289645" ID="Freemind_Link_1012609339" MODIFIED="1214293695074" POSITION="right" TEXT="Plugins applicatifs" VSHIFT="-23">
+<cloud/>
+<node CREATED="1214239208959" ID="Freemind_Link_1833358667" MODIFIED="1214239215693" TEXT="Chat"/>
+<node CREATED="1214239233383" HGAP="22" ID="Freemind_Link_1216588198" MODIFIED="1214293653115" TEXT="Cahier de textes"/>
+<node CREATED="1214239258813" HGAP="24" ID="Freemind_Link_616617149" MODIFIED="1214239309721" TEXT="Gestion de ressources" VSHIFT="-6"/>
+<node CREATED="1214239278297" HGAP="21" ID="Freemind_Link_421245117" MODIFIED="1214239585223" TEXT="Maintenance Informatique"/>
+<node CREATED="1214239322168" ID="Freemind_Link_962206321" MODIFIED="1214239329657" TEXT="Bureau p&#xe9;dagogique"/>
+<node CREATED="1214239338934" ID="Freemind_Link_1404395456" MODIFIED="1214239343691" TEXT="BcdiWeb"/>
+<node CREATED="1214239350887" ID="Freemind_Link_1416295706" MODIFIED="1214239371410" TEXT="Gestion adminsitrative B2i"/>
+</node>
+<node BACKGROUND_COLOR="#df4410" CREATED="1214292264709" ID="Freemind_Link_1180954399" MODIFIED="1214293490302" POSITION="left" TEXT="Modules syst&#xe8;me">
+<cloud/>
+<icon BUILTIN="penguin"/>
+<node CREATED="1214292332890" ID="Freemind_Link_1147083824" LINK="http://linux.crdp.ac-caen.fr/modulesLcsEtchDev/Docs/apache2ssl_html/" MODIFIED="1214293207529" TEXT="Acc&#xe8;s LCS en https"/>
+<node CREATED="1214292368787" ID="Freemind_Link_396143464" LINK="http://linux.crdp.ac-caen.fr/modulesLcsEtchDev/Docs/pla_html/" MODIFIED="1214293264387" TEXT="Explorateur d&apos;annuaire"/>
+</node>
+</node>
+</map>
