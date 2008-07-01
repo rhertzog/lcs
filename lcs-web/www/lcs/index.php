e5dd09eb3cdc370f863e730e1b8d0005efb71c14
jlcf <jlcf@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2008-07-01 08:54:44 +0000

    Add a DTD declaration and correct test for monlcs
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/BacASable/LCS2.0ndg/sources/trunk@1690 1012e355-7652-49d3-8fd6-1f9b0b464cea

--- lcs-web/www/lcs/index.php
+++ lcs-web/www/lcs/index.php
@@ -9,19 +9,21 @@ else
     die ("paramètres absents de la base de données");
 @mysql_free_result($result);
 
-if ( $url_redirect == "../squidGuard/pageinterdite.html" ) $url_accueil = $url_redirect;
-if ( $url_redirect == "accueil.php" ) 
-    if ( is_dir ("/var/www/monlcs") )
-        $url_accueil = "/monlcs/index.php" ;
-    else $url_accueil = $url_redirect;
+if ( $url_redirect == "accueil.php" || $url_redirect == "../squidGuard/pageinterdite.html" ) $url_accueil = $url_redirect;
+
+if ( $url_accueil == "accueil.php" && is_dir ("/var/www/monlcs") )  
+  $url_accueil = "/monlcs/index.php";
+
 ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">
 <html>
 <head>
-<title>...::: LCS :::...</title>
-<LINK REL="SHORTCUT ICON" HREF="images/favicon.ico" />
+  <title>...::: LCS :::...</title>
+  <link rel="SHORTCUT ICON" href="images/favicon.ico">
 </head>
-<FRAMESET ROWS="90,*" BORDER="0" >
-	<FRAME SRC="barre.php" NAME="barre" SCROLLING="no"></FRAME>
-        <FRAME SRC="<? echo"$url_accueil"; ?>" NAME="principale"></FRAME>
-</FRAMESET>
+<frameset rows="90,*" border="0">
+  <frame src="barre.php" name="barre" scrolling="no">
+  <frame src="<? echo"$url_accueil"; ?>" name="principale">
+</frameset>
 </html>
+
