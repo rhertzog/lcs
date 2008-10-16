980124f2c2dee4010aebfb1ddec7c3236b7b324b
jlcf <jlcf@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2008-10-16 16:28:03 +0000

    lcs-web: Fix bug synchro admin password in "action on the server"
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/BacASable/LCS2.0ndg/sources/trunk@1966 1012e355-7652-49d3-8fd6-1f9b0b464cea

--- lcs-web/www/Admin/action.php
+++ lcs-web/www/Admin/action.php
@@ -1,16 +1,17 @@
 <?
 /*===========================================
-   Projet LcSE3
-   Exécution d'une action sur le serveur
+   Projet LcSE3 Ex&#233;cution d'une action sur le serveur
    Admin/action.php
-   Equipe Tice académie de Caen
-   V 1.4 maj : 27/05/2004
-   Distribué selon les termes de la licence GPL
+   Equipe Tice acad&#233;mie de Caen
+   Derniere mis a jour : 16/10/2008
+   Distribue selon les termes de la licence GPL
    ============================================= */
 
 include ("../lcs/includes/headerauth.inc.php");
 include ("../Annu/includes/ldap.inc.php");
 include ("../Annu/includes/ihm.inc.php");
+
+$action = $_POST[action];
 
 function mail_to ($to, $Subject, $Body, $From) {
 	$mailHeaders = "From: $From";
@@ -20,22 +21,22 @@ function mail_to ($to, $Subject, $Body, $From) {
 list ($idpers, $login)= isauth();
 if ($idpers == "0")    header("Location:$urlauth");
 
-	echo "<HTML>\n";
-	echo "	<HEAD>\n";
-	echo "		<TITLE>...::: Interface d'administration Serveur LCS :::...</TITLE>\n";
-	echo "		<LINK  href='../Annu/style.css' rel='StyleSheet' type='text/css'>\n";
-	echo "	</HEAD>\n";
-    	echo "	<BODY>\n";
+	echo "<html>\n";
+	echo "	<head>\n";
+	echo "		<title>...::: Interface d'administration Serveur LCS :::...</title>\n";
+	echo "		<link  href='../Annu/style.css' rel='StyleSheet' type='text/css'>\n";
+	echo "	</head>\n";
+    	echo "	<body>\n";
 
 if (is_admin("system_is_admin",$login)=="Y") {
 	// Definition des messages d'alerte
 	$alerte_1=gettext("
                         <div class='error_msg'>
-                           Votre demande d'action a échouée.
-                           Si le problème persiste, veuillez contacter le super-utilisateur du serveur LCS.
+                           Votre demande d'action a &#233;chou&#233;e.
+                           Si le probl&#232;me persiste, veuillez contacter le super-utilisateur du serveur LCS.
                         </div><BR>\n");
 	// Definition des messages d'info
-	$info_1 = gettext("L'action que vous avez ordonnancée est en cours d'exécution...");
+	$info_1 = gettext("L'action que vous avez ordonnanc&#233;e est en cours d'ex&#233;cution...");
 
 	echo "<H1>Effectuer une action sur le serveur $stat_srv</H1>\n";
 	#------------------------------------------
@@ -43,13 +44,13 @@ if (is_admin("system_is_admin",$login)=="Y") {
 	if (! isset($action)) {
 		// Form de selection d'actions
 		$form = "<form action=\"action.php\" method=\"post\">\n";
-		$form .="<H3>Sélectionnez l'action à exécuter : </H3>\n";
+		$form .="<H3>S&#233;lectionnez l'action &#224; ex&#233;cuter : </H3>\n";
 		$form .= "<SELECT name='action' SIZE=1>\n";
-		$form .= "<OPTION VALUE='settime'>Mettre à l'heure le serveur</OPTION>\n";
-		$form .= "<OPTION VALUE='halt'>Arrêter le serveur</OPTION>\n";
-		$form .= "<OPTION VALUE='reboot'>Redémarrer le serveur</OPTION>\n";
-		$form .= "<OPTION VALUE='update'>Faire une mise a jour système</OPTION>\n";
-		$form .= "<OPTION VALUE='synchro_mdp'>Synchro mot de passe admin (mode sans échec)</OPTION>\n";
+		$form .= "<OPTION VALUE='settime'>Mettre &#224; l'heure le serveur</OPTION>\n";
+		$form .= "<OPTION VALUE='halt'>Arr&#234;ter le serveur</OPTION>\n";
+		$form .= "<OPTION VALUE='reboot'>Red&#233;marrer le serveur</OPTION>\n";
+		$form .= "<OPTION VALUE='update'>Faire une mise a jour syst&#232;me</OPTION>\n";
+		$form .= "<OPTION VALUE='synchro_mdp'>Synchro mot de passe admin (mode sans &#233;chec)</OPTION>\n";
 		$form .= "</SELECT>\n";
 		$form.="<input type=\"submit\" value=\"Valider\">\n";
 		$form.="</form>\n";
@@ -68,7 +69,10 @@ if (is_admin("system_is_admin",$login)=="Y") {
 		// Execution de l'action sur le serveur
 		if ( $action == "settime" )
 			exec ("/usr/bin/sudo /usr/share/lcs/scripts/action.sh $action $ntpserv" , $AllOutput, $ReturnValue);
-		else 
+		elseif ( $action == "synchro_mdp" ) {
+                        $pass = urldecode( xoft_decode($HTTP_COOKIE_VARS['LCSuser'],$key_priv) );
+			exec ("/usr/bin/sudo /usr/share/lcs/scripts/action.sh $action $pass" , $AllOutput, $ReturnValue);
+                } else
 			exec ("/usr/bin/sudo /usr/share/lcs/scripts/action.sh $action" , $AllOutput, $ReturnValue);			
 		if ($DEBUG) {
 			echo "$MsgD $ReturnValue ".count($AllOutput)."<br>";
@@ -77,16 +81,16 @@ if (is_admin("system_is_admin",$login)=="Y") {
 			}
 		}
 		if ($ReturnValue==0) {
-			$Body.= "sur le serveur LCS a été effectuée avec succès.\n";
+			$Body.= "sur le serveur LCS a ete effectuee avec succes.\n";
 			echo $info_1;
 		} else {
-			$Body.= "sur le serveur LCS à échouée.\n";
+			$Body.= "sur le serveur LCS a echouee.\n";
 			echo $alerte_1;
 		}
 		mail_to ($to, $Subject, $Body, $From);
 	} // fin traitement action
 }// fin is_admin
-else echo "Vous n'avez pas les droits nécessaires pour ordonner cette action...";
+else echo "Vous n'avez pas les droits n&#233;cessaires pour ordonner cette action...";
 
 include ("../lcs/includes/pieds_de_page.inc.php");
 ?>
