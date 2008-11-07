9035371037377a20129394c3328717edb73eafc0
misterphi <misterphi@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2008-11-07 22:36:34 +0000

    This document was successfully checked as HTML 4.01 Transitional!
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/BacASable/LCS2.0ndg/sources/trunk@2024 1012e355-7652-49d3-8fd6-1f9b0b464cea

--- lcs-web/www/Admin/eff_vide_groups.php
+++ lcs-web/www/Admin/eff_vide_groups.php
@@ -3,7 +3,7 @@
  *    Projet LCS-SE3
  *    Admin/eff_vide_groups.php
  *    David Gloux david.gloux@tice.ac-caen.fr
- *    Derniere Version  : 04/08/2007
+ *    Derniere Version  : 07/11/2008
  *    Distribué selon les termes de la licence GPL
  *    ============================================= */
 
@@ -14,15 +14,18 @@ include ("../lcs/includes/headerauth.inc.php");
 include ("../Annu/includes/ldap.inc.php");
 include ("../Annu/includes/ihm.inc.php");
 
+$phase=$_GET['phase'];
+$vides_gr=$_POST['vides_gr'];
+
 list ($idpers,$login)= isauth();
 if ($idpers == "0") header("Location:$urlauth");
 
-$html = "<HTML>\n";
+$html = "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">\n";
+$html .= " <HTML>\n";
 $html .= "      <HEAD>\n";
 $html .= "              <TITLE>...::: Interface d'administration Serveur LCS :::...</TITLE>\n";
 $html .= "              <LINK  href='../Annu/style.css' rel='StyleSheet' type='text/css'>\n";
-$html .= "      </HEAD>\n";
-$html .= "      <BODY>\n";
+
 echo $html;
 
 if (is_admin("lcs_is_admin",$login)=="Y") {
@@ -32,9 +35,9 @@ if (is_admin("lcs_is_admin",$login)=="Y") {
 	for ($loop=0; $loop < count($vides_gr); $loop++) {
 	    exec ("$scriptsbinpath/groupDel.pl $vides_gr[$loop]",$AllOutPut,$ReturnValue);
 	    if ($ReturnValue == "0") {
-		echo "<strong>Le groupe $vides_gr[$loop] a été supprimé avec succès.</strong><br>\n";
+		echo "<strong>Le groupe $vides_gr[$loop] a &#233;t&#233; supprim&#233; avec succ&#232;s.</strong><br>\n";
 	    } 
-	    else echo "<div class='error_msg'>Echec de la suppression <font color='black'>(type d'erreur : $ReturnValue)</font>, veuillez contacter <A HREF='mailto:$MelAdminLCS?subject=PB Effacement groupe'>l'administrateur du système</A></div><BR>\n";
+	    else echo "<div class='error_msg'>Echec de la suppression <font color='black'>(type d'erreur : $ReturnValue)</font>, veuillez contacter <A HREF='mailto:$MelAdminLCS?subject=PB Effacement groupe'>l'administrateur du syst&#232;me</A></div><BR>\n";
 	    
 	}
     }
@@ -44,8 +47,10 @@ if (is_admin("lcs_is_admin",$login)=="Y") {
 	if ( $phase != 1 ) {
 	    // Affichage du sablier
 	    echo "<meta HTTP-EQUIV=\"Refresh\" CONTENT=\"3;url='$PHP_SELF?phase=1'\">\n";
+	    echo "      </HEAD>\n";
+		 echo "      <BODY>\n";
 	    echo $phase;
-	    echo "<div align='center'><img src=\"Images/wait.gif\" title=\"Patientez...\" align=\"middle\" border=\"0\">&nbsp;Recherche des groupes vides en cours. Veuillez patienter...</div>";
+	    echo "<div align='center'><img src=\"Images/wait.gif\" title=\"Patientez...\" align=\"middle\" border=\"0\" alt=\"Patientez\">&nbsp;Recherche des groupes vides en cours. Veuillez patienter...</div>";
 	}
 	else {	    
 	    //recherche de tous les groupes vides
@@ -78,21 +83,21 @@ if (is_admin("lcs_is_admin",$login)=="Y") {
 	    if ( count($groupevide) == 0 )  echo "<b>Il n'existe pas de groupes vides dans votre annuaire.</b>";
 	    else {
 		echo "<form action=\"eff_vide_groups.php\" method=\"post\">";
-		echo "<B>Sélectionner le(s) groupe(s) vides que vous désirez effacer : </B><BR><BR>";
-		echo "<select name=\"vides_gr[]\" value=\"$vides_gr\" size=\"5\" multiple=\"multiple\">";
+		echo "<B>S&#233;lectionner le(s) groupe(s) vides que vous d&#233;sirez effacer : </B><BR><BR>";
+		echo "<select name=\"vides_gr[]\"  size=\"5\" multiple=\"multiple\">";
 		for ($loop=0; $loop < count ($groupevide) ; $loop++) {
 		    echo "<option value=".$groupevide[$loop].">".$groupevide[$loop];
 		}
 		echo "</select><br><br>";
 		echo " <input type=\"hidden\" name=\"valider\" value=\"$valider\">
 		       <input type=\"submit\" value=\"valider\">
-		       <input type=\"reset\" value=\"Réinitialiser la sélection\">";
+		       <input type=\"reset\" value=\"R&#233;initialiser la s&#233;lection\">";
 		echo "</form>";
 	    }
 	}
     }
 }// fin is_admin
-else echo "Vous n'avez pas les droits nécessaires pour ordonner cette action...";
+else echo "Vous n'avez pas les droits n&#233;cessaires pour ordonner cette action...";
 
 include ("../lcs/includes/pieds_de_page.inc.php");
 
