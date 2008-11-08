e2fe353645a4845c4dc764236e9818a021b56edf
misterphi <misterphi@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2008-11-08 10:47:13 +0000

    This document was successfully checked as HTML 4.01 Transitional!
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/BacASable/LCS2.0ndg/sources/trunk@2025 1012e355-7652-49d3-8fd6-1f9b0b464cea

--- lcs-imap/www/Admin/listesdiff.php
+++ lcs-imap/www/Admin/listesdiff.php
@@ -1,14 +1,18 @@
 <?php
-# /var/www/Admin/listesdiff.php derniere version du : 17/11/06
+# /var/www/Admin/listesdiff.php derniere version du : 7/11/08
 include ("../lcs/includes/headerauth.inc.php");
 include ("../Annu/includes/ldap.inc.php");
 include ("../Annu/includes/ihm.inc.php");
 
+$valideffliste=$_POST['valideffliste'] ;
+$valideaddliste=$_POST['valideaddliste'] ;
+
+
 list ($idpers, $login)= isauth();
 if ($idpers == "0") header("Location:$urlauth");
-$html = "
+$html = "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">\n 
 	  <head>\n
-	  <title>...::: Sélection briques fonctionnelles LCS  :::...</title>\n
+	  <title>...::: S&#233;lection briques fonctionnelles LCS  :::...</title>\n
 	  <link  href='../Annu/style.css' rel='StyleSheet' type='text/css'>\n
 	  </head>\n
 	  <body>\n";
@@ -23,15 +27,15 @@ if (is_admin("system_is_admin",$login)=="Y") {
 	   Liste de diffusion ldap : &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
     if (($listediff == 1 && !isset($valideffliste)) || isset($valideaddliste)) {
 	echo "<input type=\"hidden\" name=\"valideffliste\" value=\"true\">
-	       <input type=\"submit\" value=\"Désactivation\">
+	       <input type=\"submit\" value=\"D&#233;sactivation\">
 	       </form>";
-        echo "<u onmouseover=\"return escape('<strong>Désactivation des listes de  diffusion <em>LDAP</em></strong> : Désactive les listes de diffusion <em>LDAP</em>, les utilisateurs <em>LCS</em>, ne pourront plus poster des messages à des groupes.')\"><img src='../images/help-info.gif'></u>\n";
+        echo "<u onmouseover=\"return escape('<strong>D&#233;sactivation des listes de  diffusion <em>LDAP</em></strong> : D&#233;sactive les listes de diffusion <em>LDAP</em>, les utilisateurs <em>LCS</em>, ne pourront plus poster des messages &#224;des groupes.')\"><img src='../images/help-info.gif'></u>\n";
     }
     if (($listediff == 0 && !isset($valideaddliste)) || isset($valideffliste)) {
 	echo "<input type=\"hidden\" name=\"valideaddliste\" value=\"true\">
 	       <input type=\"submit\" value=\"Activation\">
 	       </form>";
-        echo "<u onmouseover=\"return escape('<strong>Mise en place des listes de diffusion <em>LDAP</em></strong> : Les listes de diffusion <strong><em>LDAP</em></strong> permettent à un utilisateur de poster un mail à l\'ensemble des membres d\'un groupe. <br /><u>Exemple</u> : Il existe dans l\'annuaire un groupe <strong>Classe_2A</strong>, il sera posssible adresser un mail à l\'ensemble des membres de ce groupe en postant un mail à l\'adresse suivante : <strong><em>Classe_2A@lcs</em></strong> .')\"><img src='../images/help-info.gif'></u>\n";
+        echo "<u onmouseover=\"return escape('<strong>Mise en place des listes de diffusion <em>LDAP</em></strong> : Les listes de diffusion <strong><em>LDAP</em></strong> permettent &#224;un utilisateur de poster un mail &#224;l\'ensemble des membres d\'un groupe. <br /><u>Exemple</u> : Il existe dans l\'annuaire un groupe <strong>Classe_2A</strong>, il sera posssible adresser un mail &#224;l\'ensemble des membres de ce groupe en postant un mail &#224;l\'adresse suivante : <strong><em>Classe_2A@lcs</em></strong> .')\"><img src='../images/help-info.gif' alt='infos'></u>\n";
     }
     if (isset($valideffliste)) {
         exec("/usr/bin/sudo /usr/share/lcs/scripts/eff_liste_diff.sh ");
@@ -51,7 +55,7 @@ if (is_admin("system_is_admin",$login)=="Y") {
     #fin liste de diffusion ldap
     
 }// fin is_admin
-else echo "Vous n'avez pas les droits nécessaires pour ordonner cette action...";
+else echo "Vous n'avez pas les droits n&#233;cessaires pour ordonner cette action...";
 echo "</div><!-- Fin container-->\n";
 include ("../lcs/includes/pieds_de_page.inc.php");
 ?>
