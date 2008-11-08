8de2c064b2b02e37e95437b35361d7cceaf70981
misterphi <misterphi@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2008-11-08 15:00:07 +0000

    save_keys.php was successfully checked as HTML 4.01 Transitional!
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/BacASable/LCS2.0ndg/sources/trunk@2026 1012e355-7652-49d3-8fd6-1f9b0b464cea

--- lcs-web/www/lcs/save_keys.php
+++ lcs-web/www/lcs/save_keys.php
@@ -1,23 +1,25 @@
-<?php   /* lcs/save_keys.php maj : 28/11/2003 */
+<?php   /* lcs/save_keys.php maj : 7/11/2008 */ 
 require "./includes/headerauth.inc.php";
 require "../Annu/includes/ldap.inc.php";
 require "../Annu/includes/ihm.inc.php";
 
 list ($idpers,$login)= isauth();
 if ($idpers == "0") header("Location:$urlauth");
+$keys=$_POST['keys'];
+echo "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">\n";
+echo "<HTML>\n";  
 ?>
-
 <head>
-<title>...:::  Sauvegarde du nouveau jeu de clés d'authentification :::...</title>
+<title>...:::  Sauvegarde du nouveau jeu de cl&#233;s d'authentification :::...</title>
 <link  href='../Annu/style.css' rel='StyleSheet' type='text/css'>
 </head>
 <body>
 <div align="center">
-        <h2>Sauvegarde du nouveau jeu de clés d'authentification</h2>
+        <h2>Sauvegarde du nouveau jeu de cl&#233;s d'authentification</h2>
 </div>
 <?php
 if ( is_admin("Lcs_is_admin",$login) == "Y" )  {
-        // Décodage de la chaine d'authentification coté serveur avec une clé privée
+        // Decodage de la chaine d'authentification cote serveur avec une cle privee
         exec ("/usr/bin/python ".$basedir."lcs/includes/decode.py '$keys'",$AllOutPut,$ReturnValue);
         $tmp = split ("[\|\]",$AllOutPut[0],5);
         $p = $tmp[0];
@@ -27,32 +29,32 @@ if ( is_admin("Lcs_is_admin",$login) == "Y" )  {
         $e = $tmp[4];
 
         if ( $p && $q && $pq && $d && $e ) {
-                // sauvegarde de la clé publique
+                // sauvegarde de la cle publique
                 $public_key="var public_key_e=[".$e."];\n";
                 $public_key.="var public_key_pq=[".$pq."];\n";
                 $fp=@fopen("public_key.js","w");
                 if($fp) {
                         fputs($fp,$public_key."\n");
                         fclose($fp);
-                        // sauvegarde de la clé privée
+                        // sauvegarde de la cle privee
                         $private_key="#[ [d], [p], [q] ]\n";
                         $private_key.="value=[[$d],[$p],[$q]]\n";
                         $fp=@fopen("includes/privateKey.py","w");
                         if($fp) {
                                 fputs($fp,$private_key."\n");
                                 fclose($fp);
-                                echo "<div align='center'>".gettext("Votre nouvelle paire de clés a été sauvegardée avec succes.")."</div>\n";
+                                echo "<div align='center'>".gettext("Votre nouvelle paire de cl&#233;s a &#233;t&#233; sauvegard&#233;e avec succ&#232;s.")."</div>\n";
                         } else {
-                                echo "<div align='center'><b>ERREUR</b> : ".gettext("Impossible de sauvegarder la nouvelle clé privée.")."</div>\n";
+                                echo "<div align='center'><b>ERREUR</b> : ".gettext("Impossible de sauvegarder la nouvelle cl&#233; priv&#233;e.")."</div>\n";
                         }
                 } else {
-                        echo "<div align='center'><b>ERREUR</b> : ".gettext("Impossible de sauvegarder la nouvelle clé publique.")."</div>\n";
+                        echo "<div align='center'><b>ERREUR</b> : ".gettext("Impossible de sauvegarder la nouvelle cl&#233; publique.")."</div>\n";
                 }
         } else {
-                echo "<div align='center'><b>ERREUR</b> : ".gettext("Impossible de sauvegarder cette paire de clés.")."</div>\n";
+                echo "<div align='center'><b>ERREUR</b> : ".gettext("Impossible de sauvegarder cette paire de cl&#233;s.")."</div>\n";
         }
 } else {
-        echo "<div class=alert_msg>".gettext("Cette fonctionnalité, nécessite les droits d'administrateur du serveur LCS !")."</div>\n";
+        echo "<div class=alert_msg>".gettext("Cette fonctionnalit&#233;, n&#233;cessite les droits d'administrateur du serveur LCS !")."</div>\n";
 }
 include ("./includes/pieds_de_page.inc.php");
 ?>
