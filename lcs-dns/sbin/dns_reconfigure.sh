fc60f1c734708e41c58cb3349b8a61e5c903c760
simc <simc@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2009-01-09 09:02:43 +0000

    change lcs-dns
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/BacASable/LCS2.0ndg/sources/trunk@2185 1012e355-7652-49d3-8fd6-1f9b0b464cea

--- lcs-dns/sbin/dns_reconfigure.sh
+++ lcs-dns/sbin/dns_reconfigure.sh
@@ -90,5 +90,6 @@ mv $IN_CONFIG_PATH/localnet.rev /etc/bind/localnet.rev
 rm /etc/resolv.conf
 echo "search $5" > /etc/resolv.conf
 echo "nameserver localhost" >> /etc/resolv.conf
-echo "nameserver $DNS_PRIMARY" >> /etc/resolv.conf
+# Disable after test
+#echo "nameserver $DNS_PRIMARY" >> /etc/resolv.conf
 invoke-rc.d bind9 force-reload  > /dev/null 2>&1
