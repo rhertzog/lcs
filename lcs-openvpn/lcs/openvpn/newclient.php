0cfdce40eb17f527fad044209feaadfd5831d9de
simc <simc@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2009-01-21 22:07:37 +0000

    lcs-openvpn : add client generation interface
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/BacASable/LCS2.0ndg/sources/trunk@2210 1012e355-7652-49d3-8fd6-1f9b0b464cea

--- lcs-openvpn/lcs/openvpn/newclient.php
+++ lcs-openvpn/lcs/openvpn/newclient.php
@@ -1,7 +1,16 @@
 <?php
-//Projet LCS
+/* =============================================
+   Projet LCS-SE3
+   Liste des profs pour openvpn
+   Equipe Tice académie de Caen
+   Derniere modifications : 21/01/2009
+   Distribué selon les termes de la licence GPL
+   ============================================= */
 include ("/var/www/lcs/includes/headerauth.inc.php");
 include ("/var/www/Annu/includes/ldap.inc.php");
+include ("/var/www/Annu/includes/ihm.inc.php");
+header_html();
+
 $msgIntro = "<H1>Gestion LCS OpenVPN</H1>\n";
 list ($idpers, $login)= isauth();
 
@@ -12,8 +21,36 @@ function mktable($title, $content) {
         echo "<h3>$title</h3>\n";
         echo $content;
 }
+if (!isset($_POST['login']))
+{
+  list ($idpers,$login)= isauth();
+  if ($idpers == "0") header("Location:$urlauth");
+  $uids = search_uids ("(cn=Profs)", "half");
+  $people = search_people_groups ($uids,"(sn=*)","cat");
+  if (count($people)) {
+    echo "Il y a ".count($people)." membre";
+    if ( count($people) >1 ) echo "s";
+    echo " dans le groupe Profs</br>\n" ;
+    echo "<form method=post>";
+    echo "<select name=login size=1>";
+    for ($loop=0; $loop < count($people); $loop++) {
+        echo "<option value=".$people[$loop]["uid"].">".$people[$loop]["fullname"]." ".$people[$loop]["sexe"]."</option>";
+    }
+        echo "</select></br><input type='submit' value='Valider' /> ";
+     echo "</form>";
+     }
+     else {
+        echo " <strong>Pas de membres</strong> dans le groupe Profs.<br>";
+     }
+}
 
+else {
+        $login=$_POST['login'];
+        echo $login;
+        exec("/usr/share/lcs/sbin/lcs-openvpn-generclient.sh $login");
+        echo "generation du certificat pour".$login;
+        echo "l utilisateur a reçu la procedure par e-mail pour utiliser le VP";
+}
 
 include ("/var/www/lcs/includes/pieds_de_page.inc.php");
-
 ?>
