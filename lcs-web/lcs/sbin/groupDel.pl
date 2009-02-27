32f05f6169f751ead170579a7a83ff0a8fa33e20
jlcf <jlcf@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2009-02-27 17:06:40 +0000

    lcs-web : groupAdd.pl, groupDel.pl, userAdd.pl modify for CyberSchool integration
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/BacASable/LCS2.0ndg/sources/trunk@2253 1012e355-7652-49d3-8fd6-1f9b0b464cea

--- lcs-web/lcs/sbin/groupDel.pl
+++ lcs-web/lcs/sbin/groupDel.pl
@@ -8,4 +8,8 @@ $dn = "cn=$cn,$groupsDn";
 
 $res = system("/usr/share/lcs/sbin/entryDel.pl $dn");
 
-exit O;
+# Generate samba's configuration (group's shares)
+`sudo /usr/share/lcs/scripts/execution_script_plugin.sh /usr/share/lcs/sbin/lcs-smb-config &>/dev/null &` 
+	if(-x "/usr/share/lcs/sbin/lcs-smb-config");
+
+exit 0;
