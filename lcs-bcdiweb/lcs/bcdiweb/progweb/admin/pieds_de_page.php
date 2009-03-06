123543fa19b2c25163414fe33babdf3a0ef2b47f
simc <simc@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2009-03-06 22:29:45 +0000

    lcs-bcdiweb : bugfix
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/BacASable/LCS2.0ndg/sources/trunk@2276 1012e355-7652-49d3-8fd6-1f9b0b464cea

--- lcs-bcdiweb/lcs/bcdiweb/progweb/admin/pieds_de_page.php
+++ lcs-bcdiweb/lcs/bcdiweb/progweb/admin/pieds_de_page.php
@@ -1,6 +1,6 @@
 </DIV><!-- Fin du container -->  
   <DIV class="pdp">
-    Copyleft &copy; 2001 Plugins LCS «<?php echo $NAME_PLUG; ?>» Version : <?php echo $VER_PLUG ; ?>
+    Copyleft &copy; 2008-2009 Module LCS «<?php echo $NAME_PLUG; ?>» Version : <?php echo $VER_PLUG ; ?>
   </DIV>
 <script language='Javascript' type='text/javascript' src='Includes/wz_tooltip.js'></script>
 </BODY>
