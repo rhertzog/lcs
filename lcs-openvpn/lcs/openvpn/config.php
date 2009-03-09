2f4b2bcfe5b3c7247dad2b595e57efc43fa97953
simc <simc@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2009-03-09 23:36:47 +0000

    lcs-openvpn : lot of change see changelog
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/BacASable/LCS2.0ndg/sources/trunk@2282 1012e355-7652-49d3-8fd6-1f9b0b464cea

--- lcs-openvpn/lcs/openvpn/config.php
+++ lcs-openvpn/lcs/openvpn/config.php
@@ -7,83 +7,45 @@ $msgIntro = "<H1>Gestion LCS OpenVPN</H1>\n";
 list ($idpers, $login)= isauth();
 
 if (ldap_get_right("lcs_is_admin",$login)!="Y")
-  die (gettext("Vous n'avez pas les droits suffisants pour acc�der � cette fonction")."</BODY></HTML>");
+  die (gettext("Vous n'avez pas les droits suffisants pour acci?1/2der i?1/2 cette fonction")."</BODY></HTML>");
 
 function mktable($title, $content) {
         echo "<h3>$title</h3>\n";
         echo $content;
 }
 
-function aff_param_form($cat)
-{
-        $texte_form="<table border=\"0\">";
-        $result=mysql_query("SELECT * from params WHERE cat=$cat ORDER BY `id`");
+if (!isset($_POST['submit'])) {
+        echo "Parametre de Lcs-OpenVPN ";
+        echo "<table border=\"0\"><form method=post>";
+        $result=mysql_query("SELECT * from lcs_db.params WHERE id between 100 and 106 ORDER BY `id`");
         if ($result) {
                 while ($r=mysql_fetch_array($result)) {
-                        $texte_form .= "<tr><td colspan=\"2\">".$r["descr"]." (<em><font color=\"red\">".$r["name"]."</font></em>)</td>";
-                        $texte_form .= "<td><input TYPE=\"text\" size=\"50\" value=\"".$r["value"]."\" NAME=\"form_".$r["name"]."\"</td></tr>\n";
+                        echo "<tr><td colspan=\"2\">".$r["descr"]." (<em><font color=\"red\">".$r["name"]."</font></em>)</td>";
+                        echo "<td><input type=\"text\" size=\"50\" value=\"".$r["value"]."\" name=\"form_".$r["name"]."\"</td></tr>\n";        
+                //echo "<td><input TYPE=\"text\" size=\"50\" value=\"".$r["value"]."\" NAME=\"".$r["name"]."</td></tr>\n";
                 }
         }
-        $texte_form .= "</TABLE>";
-        return $texte_form;
+        echo "</table>";
+        echo "<input type='submit' value='Valider'>";
+        echo "<input type='hidden' value=1 name='submit'></form>";
 }
 
+if (isset($_POST['submit'])) {
+        echo "Traitement des modification";
+        echo "le Service openvpn va redemarrer pour prendre en compte les modifications";
+        $vpnnet==$_POST['vpnnet'];
+        $vpnnetmasq==$_POST['vpnnetmasq'];
+        $vpnport==$_POST['vpnport'];
+        $vpncert==$_POST['vpncert'];
+        $vpndh==$_POST['vpndh'];
+        $vpnvlan==$_POST['vpnvlan'];
+	$vpnconnexion==$_POST['vpnconnexions'];
+        #exec("sudo /usr/share/lcs/sbin/lcs-openvpn-reconfig.sh $vpnnet, $vpnnetmasq, $vpnport, $vpncert, $vpndh, $vpnvlan $vpnconnexions");
+        echo "$vpnnet, $vpnnetmasq, $vpnport, $vpncert, $vpndh, $vpnvlan $vpnconnexions";
 
-if (!isset($cat)) $cat=100;
 
-if ((!isset($submit)) and (!isset($queri))) {
-// Affichage du form de mise � jour des param�tres
-        print "<form method=\"post\">\n";
-        if ( $cat==0 || $cat==100 )
-                mktable("Paramétres OpenVPN",aff_param_form(100));
-        if ( $cat==0 || $cat==200 )
-                mktable("Paramétres xxxx",aff_param_form(200));
-        print "<be><dev align=\"center\"><INPUT TYPE=\"submit\" value=\"".gettext("Valider")."\"></div>";
-        print "<input type=\"hidden\" value=\"$cat\" name=\"submit\">";
-        print "</form>\n";
-}
-if (isset($submit)) {
-// Traitement du Form
-
-// Détection des paramètres modifiés et fabrication de la requete de mise a jour
-        if ( ( file_exists ("/tmp/params_lcs") ) ||  !( $fp = fopen("/tmp/params_lcs", "w") ) )
-                die (gettext("Création du fichier de passage des parametres impossible. Recommencez plus tard et assurez-vous qu'aucun fichier params_lcs n'est présent dans ")."/tmp.");
-        $query="SELECT * from params";
-        if ($submit != 0) $query .= " WHERE cat=$submit";
-        $result=mysql_query($query);
-        if ($result) {
-                $i=0;
-                while ($r=mysql_fetch_array($result)) {
-                        $formname="form_".$r["name"];
-                        if ($$formname!=$r["value"]) {
-                        // Mise à jour de la base de données
-                                $queri="UPDATE params SET value=\"".$$formname."\" WHERE name=\"".$r["name"]."\"";
-                                $result1=mysql_query($queri);
-                                if ($result1)
-                                        print gettext("Modification du paramètre ").
-                                        "<em><font color=\"red\">".$r["name"].
-                                        "</font></em> ". gettext("de ")."<strong>".$r["value"].
-                                        "</strong>".gettext(" en ")."<strong>".$$formname."</strong>"."<br>\n";
-                                else
-                                        print gettext("oops: la requete ") . "<strong>$queri</strong>" . gettext(" a provoqué une erreur");
-                                // Récupération des variables qui ont changées et qui nécessitent une modification des fichiers de conf
-                        }
-                }
-
-                if ($i == 0) print gettext("Aucun paramètre n'a été modifié\n");
-
-                mysql_free_result($result);
-        } else print gettext ("oops: Erreur inattendue de lecture des anciens paramétres\n");
-        fclose($fp);
-        // Effacement du fichier si rien n'y a été inscrit
-        if (filesize("/tmp/params_lcs")!=0) {
-                # execution du script de modification
-                exec ("/usr/bin/sudo /usr/share/lcs/scripts/edit_params.sh");
-                print gettext("les fichiers de configuration ont été modifiés afin de prendre en compte les nouveaux paramétres.");
-        } else unlink ("/tmp/params_lcs");
 }
 
 
 include ("/var/www/lcs/includes/pieds_de_page.inc.php");
-
 ?>
