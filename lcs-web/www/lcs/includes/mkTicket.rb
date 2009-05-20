ee60b8297c9a6f3899a97664062df3423cb5c266
jlcf <jlcf@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2009-05-20 11:45:30 +0000

    lcs-web : Find path for rubycas-lcs and isaac in mkTicket.rb
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/LCS/LCS2.0ndg/sources/trunk@2460 1012e355-7652-49d3-8fd6-1f9b0b464cea

--- lcs-web/www/lcs/includes/mkTicket.rb
+++ lcs-web/www/lcs/includes/mkTicket.rb
@@ -1,18 +1,33 @@
 #!/usr/bin/ruby
-	require 'rubygems'
-	$gem_path = Gem.path[-1]
-	$RCS_PATH = "#{$gem_path}/gems/rubycas-lcs-0.7.1"
-	$: << $RCS_PATH + "/vendor/isaac_0.9.1"
+        require 'rubygems'
 
-	require 'crypt/ISAAC'
+       d = Dir.open("#{Gem.path[-1]}/gems/")
+       d = d.sort
+       d.each { |x|
+               if (/rubycas-lcs/.match(x))
+                       @gem = x.to_s
+               end
+       }
 
-	def random_string(max_length = 29)
-	      rg =  Crypt::ISAAC.new
-	      max = 4294619050
-	      r = "#{Time.now.to_i}r%X%X%X%X%X%X%X%X" % 
-	        [rg.rand(max), rg.rand(max), rg.rand(max), rg.rand(max), 
-	         rg.rand(max), rg.rand(max), rg.rand(max), rg.rand(max)]
-	      r[0..max_length-1]
-	end
-	ticket = random_string
-	puts "#{ticket}"
+       d = Dir.open("#{Gem.path[-1]}/gems/#{@gem}/vendor")
+       d = d.sort
+       d.each { |x|
+               if (/isaac_/.match(x))
+                       @gem2 = x.to_s
+               end
+       }
+
+$: << "#{Gem.path[-1]}/gems/#{@gem}/vendor/#{@gem2}"
+
+        require 'crypt/ISAAC'
+
+        def random_string(max_length = 29)
+              rg =  Crypt::ISAAC.new
+              max = 4294619050
+              r = "#{Time.now.to_i}r%X%X%X%X%X%X%X%X" %
+                [rg.rand(max), rg.rand(max), rg.rand(max), rg.rand(max),
+                 rg.rand(max), rg.rand(max), rg.rand(max), rg.rand(max)]
+              r[0..max_length-1]
+        end
+        ticket = random_string
+        puts "#{ticket}"
