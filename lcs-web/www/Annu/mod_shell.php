1fc46ba8b85e068786630222478b26517bcee6d1
jlcf <jlcf@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2009-05-28 11:35:39 +0000

    lcs-web :  Modify mod_shell.php
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/LCS/LCS2.0ndg/sources/trunk@2498 1012e355-7652-49d3-8fd6-1f9b0b464cea

--- lcs-web/www/Annu/mod_shell.php
+++ lcs-web/www/Annu/mod_shell.php
@@ -6,7 +6,7 @@
    [LCS CoreTeam]
    « jLCF >:> » jean-luc.chretien@tice.ac-caen.fr
    Equipe Tice academie de Caen
-   Derniere modification : 02/12/07
+   Derniere modification : 28/05/09
    ============================================= */
   include "../lcs/includes/headerauth.inc.php";
   include "includes/ldap.inc.php";
@@ -26,7 +26,7 @@ elseif ( isset($_GET['shell_mod'])) $shell_mod = $_GET['shell_mod'];
     
   // en-tete
   $html = "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">\n";
-  $html  = "<HTML>\n";
+  $html .= "<HTML>\n";
   $html .= "	<HEAD>\n";
   $html .= "		<TITLE>...::: Interface d'administration Serveur LCS :::...</TITLE>\n"; 
   $html .= "            <META HTTP-EQUIV=\"Content-Type\" CONTENT=\"tetx/html; charset=ISO-8859-1\">\n";
@@ -34,47 +34,50 @@ elseif ( isset($_GET['shell_mod'])) $shell_mod = $_GET['shell_mod'];
   // Redirection vers phase suivante, gestion du «sablier»
   if( $phase == 1 )
 	echo "<META HTTP-EQUIV=\"Refresh\" CONTENT=\"1;url='$PHP_SELF?phase=2&shell_orig=$shell_orig&shell_mod=$shell_mod'\">\n";
+
   $html  = "		<LINK  href='style.css' rel='StyleSheet' type='text/css'>\n";
   $html .= "	</HEAD>\n";
   $html .= "	<BODY>\n";
-  $html .= "<DIV>\n";
+  $html .= "<div>\n";
   echo $html;  
   aff_trailer ("1");
   if (is_admin("Annu_is_admin",$login)=="Y") {
     if ( !isset($phase) ) {
-        $html = "<H3>Modification du shell des utilisateurs :</H3>\n";
+        $html = "<h3>Modification du shell des utilisateurs :</h3>\n";
         // Affichage du formulaire de selection du shell
-        $html .= "<DIV style=\"margin-left: 50px;\">\n";
+        $html .= "<div style=\"margin-left: 50px;\">\n";
         $html .= "<form name = \"shellmod\" action=\"mod_shell.php\" method=\"post\">\n";
         $html .= "de \n";
         $html .= "<select name=\"shell_orig\">\n";
         $html .= "  <option>/bin/bash</option>\n";
         $html .= "  <option selected>/bin/true</option>\n";
-        $html .= "  <option>/usr/lib/sftp-server</option>\n";
+        //$html .= "  <option>/usr/lib/sftp-server</option>\n";
 	$html .= "</select> \n"; 
         $html .= "en \n";
         $html .= "<select name=\"shell_mod\">\n";
         $html .= "  <option>/bin/bash</option>\n";
         $html .= "  <option>/bin/true</option>\n";
-        $html .= "  <option selected>/usr/lib/sftp-server</option>\n";
+        //$html .= "  <option selected>/usr/lib/sftp-server</option>\n";
 	$html .= "</select> \n";   
         $html .= "<input type=\"hidden\" name=\"phase\" value=\"1\">\n";
         $html .= "<input type=\"submit\" value=\"Valider\">\n";          
         $html .= "</form>\n";
         // Message d'information
-        $html .= "<DIV style=\"margin-top: 50px; margin-right: 50px;\">\n";
+        $html .= "<div style=\"margin-top: 50px; margin-right: 50px;\">\n";
         $html .= "<img src=\"images/warning.png\" align=\"left\" alt=\"message d'alerte\">\n";
         $html .= "Attention cette modification concernera tous les utilisateurs !\n";
-        $html .= "<P>Ainsi, si vous demandez la modification du shell <tt class=\"shell\">/bin/true</tt> vers le shell\n";
-        $html .= "<tt class=\"shell\">/usr/lib/sftp-server</tt>, c'est l'ensemble des comptes pourvu du shell\n";
-        $html .= "<tt class=\"shell\">/bin/true</tt> qui se verra affect&#233; le shell <tt class=\"shell\">/usr/lib/sftp-server</tt>.<br>\n";
-        $html .= "Mesurer donc bien les cons&#233;quences de cette action avant de la valider.</P>\n";
+        $html .= "<p>Ainsi, si vous demandez la modification du shell <tt class=\"shell\">/bin/true</tt> vers le shell\n";
+        $html .= "<tt class=\"shell\">/bin/bash</tt>, c'est l'ensemble des comptes pourvu du shell\n";
+        $html .= "<tt class=\"shell\">/bin/true</tt> qui se verra affect&#233; le shell <tt class=\"shell\">/bin/bash</tt>.<br>\n";
+        $html .= "Mesurez donc bien les cons&#233;quences de cette action avant de la valider.</p>\n";
         $html .= "<u>Signification des diff&#233;rents shell</u> :\n";
-        $html .= "<li> <tt class=\"shell\">/bin/true</tt> : l'utilisateur ne poss&#232;de pas de shell sur le serveur,\n";
-        $html .= "<li> <tt class=\"shell\">/usr/lib/sftp-server</tt> : l'utilisateur acc&#232;de &#224; son espace perso. en sftp sur le port 2222,\n";
-        $html .= "<li> <tt class=\"shell\">/bin/bash</tt> : l'utilisateur dispose d'un shell bash sur le serveur.\n";            
-        $html .= "</DIV>\n";
-        $html .= "</DIV>\n";
+        $html .= "<ul>\n<li> <tt class=\"shell\">/bin/true</tt> : l'utilisateur ne poss&#232;de pas de shell sur le serveur,</li>\n";
+        //$html .= "<li> <tt class=\"shell\">/usr/lib/sftp-server</tt> : l'utilisateur acc&#232;de &#224; son espace perso. en sftp sur le port 2222,\n";
+        $html .= "<li> <tt class=\"shell\">/bin/bash</tt> : l'utilisateur dispose d'un shell bash sur le serveur.</li>\n</ul>\n";
+        $html .= "<u>Autoriser l'acc&#232;s <strong>sftp</strong> aux utilisateurs</u> :\n";
+        $html .= "<p>Pour autoriser l'acc&#232;s <strong>sftp</strong>, ind&#233;pendemment du shell, il faudra ajouter chaque utilisateur dans le groupe <strong>sftp-allowed</strong>.</p>\n";
+        $html .= "</div>\n";
+        $html .= "</div>\n";
         echo $html;
     }
     if ( $phase == 1 ) {
@@ -87,7 +90,7 @@ elseif ( isset($_GET['shell_mod'])) $shell_mod = $_GET['shell_mod'];
             if ($ReturnValue == "0")
 	      echo "La modification du shell des utilisateurs de <tt class=\"shell\">$shell_orig</tt> vers <tt class=\"shell\">$shell_mod</tt> s'est d&#233;roul&#233;e avec succ&#232;s.<br>";
 	    else
-              echo "<div class=error_msg>Echec de la modification du shell des utilisateurs  !</div>";           
+              echo "<div class=error_msg>Echec de la modification du shell des utilisateurs !</div>";           
            } else    
               echo "<div class=error_msg>Il faut s&#233;lectionner deux shells diff&#233;rents !</div>";
     }  
