1485bbba549bcb1f2065012d2a6261fb854af80b
misterphi <misterphi@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2009-06-25 20:26:41 +0000

    lcs-web : add error message in gestpack.sh (by sandrine)
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/LCS/LCS2.0ndg/sources/trunk@2622 1012e355-7652-49d3-8fd6-1f9b0b464cea

--- lcs-web/lcs/scripts/gestpack.sh
+++ lcs-web/lcs/scripts/gestpack.sh
@@ -4,61 +4,61 @@
 # par Misterphi le 31/03/2009
 # gestion des modules par interface web
 # scripts d'apt-get sudoifie
-# paramètres passés :	$1=source du paquet; 
+# paramÃ¨tres passÃ©s :	$1=source du paquet; 
 #				$2= action (install/remove);
 #				$3= nom du paquet
 # ====================================================================
 
-# on positionne les drapeaux indiquant le début de l'opération. flip nécessaire pour la page auto refresh 
+# on positionne les drapeaux indiquant le dÃ©but de l'opÃ©ration. flip nÃ©cessaire pour la page auto refresh 
 echo "<? \$flip=0;\$verrou=1;?>" > /usr/share/lcs/Modules/flag.php
 
 #on recherche un depot similaire a $1
 Depot=`grep \`echo $1 | cut -d"/" -f3\` /etc/apt/sources.list`
 if [ -n "$Depot" ];then
- 
+
 	if [ "$2" = "install" ]; then
-   cp /etc/apt/sources.list /etc/apt/sources.tmp
-   sed -i "s#$Depot#$1#g" /etc/apt/sources.list
-    fi
+  cp /etc/apt/sources.list /etc/apt/sources.tmp
+  sed -i "s#$Depot#$1#g" /etc/apt/sources.list
+   fi
 
-    #on simule une cli dans la page web
-    echo " <TABLE WIDTH=70% BORDER=2 ALIGN=CENTER><TR><TD WIDTH=50% ALIGN=TOP BGCOLOR='#000000'><FONT FACE='Arial, sans-serif'COLOR='#FFFFFF'><FONT SIZE=2>" >> /tmp/ecran_install_$3.html 
-    echo "$2 $3" | sed -e "s/$/<BR>/g" >> /tmp/ecran_install_$3.html 
+   #on simule une cli dans la page web
+   echo " <TABLE WIDTH=70% BORDER=2 ALIGN=CENTER><TR><TD WIDTH=50% ALIGN=TOP BGCOLOR='#000000'><FONT FACE='Arial, sans-serif'COLOR='#FFFFFF'><FONT SIZE=2>" >> /tmp/ecran_install_$3.html 
+   echo "$2 $3" | sed -e "s/$/<BR>/g" >> /tmp/ecran_install_$3.html 
 
-    #on initialise dpkg au cas où un processus apt aurait foiré précédemment
-    dpkg --configure -a
+   #on initialise dpkg au cas oÃ¹ un processus apt aurait foirÃ© prÃ©cÃ©demment
+   dpkg --configure -a
 
-    #on lance le processus
-    apt-get update | sed -e "s/$/<BR>/g" >>  /tmp/ecran_install_$3.html 
-    apt-get $2 -y $3 | sed -e "s/$/<BR>/g" >>  /tmp/ecran_install_$3.html
+   #on lance le processus
+   apt-get update | sed -e "s/$/<BR>/g" >>  /tmp/ecran_install_$3.html 
+   apt-get $2 -y $3 | sed -e "s/$/<BR>/g" >>  /tmp/ecran_install_$3.html
 
-    #on teste l'état du paquet, et on affiche le résultat
-    vartest=`dpkg -l $3 | grep ii | wc -l`
-    if [ $vartest -eq 0 ]; then
-        if [ "$2" = "install" ];then
-            echo "<FONT color='#FF0000'>Le paquet $3 n'est <B>PAS OPERATIONNEL</B></FONT>" | sed -e "s/$/<BR><BR>/g" >>  /tmp/ecran_install_$3.html
-        else echo "<BR><FONT color='#FF0000'>Le paquet $3 est <B>DESINSTALLE</B> </FONT>" | sed -e "s/$/<BR><BR>/g" >>  /tmp/ecran_install_$3.html
-        fi
-    else echo "<FONT color='#33FF33'> Le paquet $3 est <B>OPERATIONNEL</B> </FONT>" | sed -e "s/$/<BR><BR>/g" >>  /tmp/ecran_install_$3.html
-    fi
+   #on teste l'Ã©tat du paquet, et on affiche le rÃ©sultat
+   vartest=`dpkg -l $3 | grep ii | wc -l`
+   if [ $vartest -eq 0 ]; then
+       if [ "$2" = "install" ];then
+           echo "<FONT color='#FF0000'>Le paquet $3 n'est <B>PAS OPERATIONNEL</B></FONT>" | sed -e "s/$/<BR><BR>/g" >>  /tmp/ecran_install_$3.html
+       else echo "<BR><FONT color='#FF0000'>Le paquet $3 est <B>DESINSTALLE</B> </FONT>" | sed -e "s/$/<BR><BR>/g" >>  /tmp/ecran_install_$3.html
+       fi
+   else echo "<FONT color='#33FF33'> Le paquet $3 est <B>OPERATIONNEL</B> </FONT>" | sed -e "s/$/<BR><BR>/g" >>  /tmp/ecran_install_$3.html
+   fi
 
-    #pour le fun, on affiche une invite 	
-    echo "www-data@Lcs:/$<blink><B>_</B></blink>" | sed -e "s/$/<BR>/g" >>  /tmp/ecran_install_$3.html
-    echo "</FONT></FONT></TD></TR></TABLE>" >> /tmp/ecran_install_$3.html 
+   #pour le fun, on affiche une invite 	
+   echo "www-data@Lcs:/$<blink><B>_</B></blink>" | sed -e "s/$/<BR>/g" >>  /tmp/ecran_install_$3.html
+   echo "</FONT></FONT></TD></TR></TABLE>" >> /tmp/ecran_install_$3.html 
 
-    #on restaure le sourceslist d'origine
-    if [ "$2" = "install" ];then
-        mv  /etc/apt/sources.tmp /etc/apt/sources.list
-    fi
+   #on restaure le sourceslist d'origine
+   if [ "$2" = "install" ];then
+       mv  /etc/apt/sources.tmp /etc/apt/sources.list
+   fi
 
-    #on initialise dpkg si le processus a foiré
-    dpkg --configure -a
+   #on initialise dpkg si le processus a foirÃ©
+   dpkg --configure -a
 
-    #pour que le script puisse effacer le fichier temporaire
-    chown www-data:www-data /tmp/ecran_install_$3.html
-
+   #pour que le script puisse effacer le fichier temporaire
+   chown www-data:www-data /tmp/ecran_install_$3.html
+else echo "<BR><FONT color='#FF0000'>D&eacute;p&ocirc;t $1 non trouv&eacute; dans la liste des d&eacute;p&ocirc;ts ; veuillez contacter l'administrateur du r&eacute;seau.</FONT>" | sed -e "s/$/<BR><BR>/g" >> /tmp/ecran_install_$3.html
 fi
-#on positionne les drapeaux indiquant la FIN de l'opération, flip pour arrêter l'auto refresh
+#on positionne les drapeaux indiquant la FIN de l'opÃ©ration, flip pour arrÃªter l'auto refresh
 echo "<? \$flip=1;\$verrou=0;?>" > /usr/share/lcs/Modules/flag.php
 
 # on at la raz de $flip  en cas de pb sur la page autorefresh
