114e141e132eb6bdb106032772465ef3d21f5469
jlcf <jlcf@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2009-10-16 13:37:58 +0000

    lcs-smbwebclient : Add smbweb_is_open and password policy acces
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/LCS/LCS2.0ndg/sources/trunk@2825 1012e355-7652-49d3-8fd6-1f9b0b464cea

--- lcs-smbwebclient/conf/Maj/maj1.sh
+++ lcs-smbwebclient/conf/Maj/maj1.sh
@@ -5,7 +5,9 @@
 #
 # Inscription du params shareview dans lcs_db.params
 #
-echo "Inscription du params shareview dans lcs_db.params"
-/usr/bin/mysql -e "INSERT INTO lcs_db.params VALUES (NULL, 'shareview', 'Docs/Classes/Progs', '0', 'Partages Visibles avec smbwebclient', '1');"
+if [ `/usr/bin/mysql -se "SELECT id FROM lcs_db.params WHERE name='shareview';" | wc -l` == "0" ]; then
+    echo "Inscription du params shareview dans lcs_db.params"
+    /usr/bin/mysql -e "INSERT INTO lcs_db.params VALUES (NULL, 'shareview', 'Docs/Classes/Progs', '0', 'Partages Visibles avec smbwebclient', '1');"
+fi
 # On efface l'entrée share_view utilisée pour les tests
 /usr/bin/mysql -e "DELETE FROM lcs_db.params WHERE name = 'share_view' ;"
