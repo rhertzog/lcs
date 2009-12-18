bd5a8b06a40069c049f66f3524ef07fa6f992aa1
simc <simc@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2009-12-18 13:42:18 +0000

    lcs-caenstats : initial release
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/LCS/LCS2.0ndg/sources/trunk@3115 1012e355-7652-49d3-8fd6-1f9b0b464cea

---
+++ lcs-caenstats/debian/lcs-caenstats-default.ex
@@ -0,0 +1,10 @@
+# Defaults for lcs-caenstats initscript
+# sourced by /etc/init.d/lcs-caenstats
+# installed at /etc/default/lcs-caenstats by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
