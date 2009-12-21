cbcb6c78efe89610e3c70e7d86c73116a02dd83a
mrt <mrt@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2009-12-21 10:19:42 +0000

    lcs-helpdesk: retrait js caduque
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/LCS/LCS2.0ndg/sources/trunk@3123 1012e355-7652-49d3-8fd6-1f9b0b464cea

--- lcs-helpdesk/helpdesk/index.php
+++ lcs-helpdesk/helpdesk/index.php
@@ -39,7 +39,6 @@ body	{
 	<script type='text/javascript' src='/lib/js/ext/ext-all-debug.js'></script>
 	<script type='text/javascript' src='/helpdesk/js/RowExpander.js'></script>
 	<script type="text/javascript" src="/helpdesk/js/helpdesk.js"></script>
-	<script type="text/javascript" src="/helpdesk/js/upload.js"></script>
 	<script type='text/javascript'>
 		function init() {
 			doHelpDesk(true);
