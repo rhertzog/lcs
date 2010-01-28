f46087637826a7b71695daa5777e907aff5bf041
misterphi <misterphi@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2010-01-28 12:35:57 +0000

    lcs-gepi : fixing path to doc and lower name in prerm
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/LCS/LCS2.0ndg/sources/trunk@3186 1012e355-7652-49d3-8fd6-1f9b0b464cea

--- lcs-gepi/conf/Scripts/chown.sh
+++ lcs-gepi/conf/Scripts/chown.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-chown -R root:root /usr/share/doc/lcs/$1
+module=`echo $1 | tr [:upper:] [:lower:]`
+chown -R root:root /usr/share/doc/lcs/$module
 chown -R root:root /var/lib/lcs/$1
 chown -R www-data:www-data /usr/share/lcs/Plugins/$1
 chmod 750 /usr/share/lcs/Plugins/$1
