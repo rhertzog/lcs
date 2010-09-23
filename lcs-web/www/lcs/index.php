9dd7325d089187a89c0c4b0411356a7d0080af7b
jlcf <jlcf@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2010-09-23 11:14:11 +0000

    lcs-web : Add test for lcs-desktop to verify if browser is not ie5 or ie6
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/LCS/LCS2.0ndg/sources/trunk@3543 1012e355-7652-49d3-8fd6-1f9b0b464cea

--- lcs-web/www/lcs/index.php
+++ lcs-web/www/lcs/index.php
@@ -10,13 +10,14 @@ else
     die ("parametres absents de la base de donnees");
 @mysql_free_result($result);
 
-if ( $desktop == 1 ) header("Location:../desktop");
-
+// Redirects IE5 and IE6 users.
+$ua = $_SERVER['HTTP_USER_AGENT'];
+$ie5 = strpos($ua, 'MSIE 5') !== false;
+$ie6 = strpos($ua, 'MSIE 6') !== false;
+// Redirects to desktop if not IE.
+if ( $desktop ==1 && ! $ie5 && ! $ie6 ) header("Location:../desktop/");
 if ( $url_redirect == "accueil.php" || $url_redirect == "../squidGuard/pageinterdite.html" ) $url_accueil = $url_redirect;
-
-if ( $url_accueil == "accueil.php" && $monlcs== 1 )  
-  $url_accueil = "/monlcs/index.php";
-
+if ( $url_accueil == "accueil.php" && $monlcs== 1 ) $url_accueil = "/monlcs/index.php";
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">
 <html>
