6205605cb4bb83c105847774f39f4a038125261e
misterphi <misterphi@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2010-10-07 08:38:07 +0000

    lcs-bcdiweb : remove old version
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/LCS/LCS2.0ndg/sources/trunk@3572 1012e355-7652-49d3-8fd6-1f9b0b464cea

--- lcs-bcdiweb/lcs/Plugins/bcdiweb/progweb/admin/pieds_de_page.php
+++
@@ -1,7 +0,0 @@
-</DIV><!-- Fin du container -->  
-  <DIV class="pdp">
-    Copyleft &copy; 2008-2009 Module LCS «<?php echo $NAME_PLUG; ?>» Version : <?php echo $VER_PLUG ; ?>
-  </DIV>
-<script language='Javascript' type='text/javascript' src='Includes/wz_tooltip.js'></script>
-</BODY>
-</HTML>
