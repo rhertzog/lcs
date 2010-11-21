2b214c78b1f0cfbd8cc233935136e5e6afd15d25
misterphi <misterphi@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2010-11-21 13:23:25 +0000

    lcs-cdt : add xml_character_encode in tag  title
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/LCS/LCS2.0ndg/sources/branches/2.4.8@3630 1012e355-7652-49d3-8fd6-1f9b0b464cea

--- lcs-cdt/Cdt/scripts/flux_rss.php
+++ lcs-cdt/Cdt/scripts/flux_rss.php
@@ -212,9 +212,8 @@ if (count($mattaf)>0)
 	{
 for ($loop=0; $loop < count($dattaf) ; $loop++)
  {
-   echo 
-'<item>
-  <title>'.$mattaf[$loop].' :  '.$preftaf[$loop] .'  '. $proftaf[$loop].'</title>
+echo '<item>
+  <title>'.stripslashes(xml_character_encode($mattaf[$loop])).' :  '.$preftaf[$loop] .'  '. $proftaf[$loop].'</title>
   <description> Pour le '.$dattaf[$loop]."/&lt;br /&gt;".stripslashes(xml_character_encode($texttaf[$loop])).' </description>
   <link>http://'.$hostn.'/Plugins/Cdt/scripts/poprss.php?id='.$idtafcrypt[$loop].'</link>
 </item>'. "\n";
@@ -223,8 +222,7 @@ for ($loop=0; $loop < count($dattaf) ; $loop++)
 }
 else
 {
-	echo 
-'<item>
+echo '<item>
   <title>Rien de programmé !</title>
   <description> Mais en cherchant bien ...</description>
   <link>http://'.$hostn.'/</link>
