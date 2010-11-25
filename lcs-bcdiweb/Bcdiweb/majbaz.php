b983153f68ed5b5f81f9884dab3c33c3b13c9d54
misterphi <misterphi@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2010-11-25 13:09:32 +0000

    lcs-bcdiweb : change rights on /etc/cron.d/bcdiweb, display callback after update datas
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/LCS/LCS2.0ndg/sources/branches/2.4.8@3638 1012e355-7652-49d3-8fd6-1f9b0b464cea

--- lcs-bcdiweb/Bcdiweb/majbaz.php
+++ lcs-bcdiweb/Bcdiweb/majbaz.php
@@ -1,7 +1,7 @@
-<?
+<?php
 /* =============================================
    Projet LCS-SE3
-   Distribué selon les termes de la licence GPL
+   Distribue selon les termes de la licence GPL
    ============================================= */
    
 /* $Id: sauv.php 626 2005-10-25 07:59:18Z plouf $ */
@@ -17,7 +17,7 @@ if (isset($_GET["rsync"])) {
 	system("$PTP/scripts/syncbcdi.sh");
 } else if (isset($_GET["cron"])) {
 	system("echo \"$min $hour * * 0-5 www-data  /usr/share/lcs/Plugins/Bcdiweb/scripts/syncbcdi.sh\" > /etc/cron.d/bcdiweb");
-	echo "L'heure de mise &#224; jour quotidienne a &#233;t&#233; enregistr&#233;e .";
+	echo "L'heure de mise &#224; jour  a &#233;t&#233; enregistr&#233;e .";
 } 
 					       
 /***************************************************************************************************/
@@ -25,18 +25,19 @@ $set_minute=exec("if [ -f /etc/cron.d/bcdiweb ]; then cut -f 1 -d \" \" /etc/cro
 $set_hour=exec("if [ -f /etc/cron.d/bcdiweb ]; then cut -f 2 -d \" \" /etc/cron.d/bcdiweb; fi");
 
 echo "<form method=\"get\" action=\"majbaz.php\">";
-echo "<H2>Mise a jour manuelle</H2>";
-echo "Mise a jour manuelle imm&eacute;diate de la base Data sur Lcs";
+echo "<H2>Mise &#224; jour manuelle</H2>";
+echo "Mise &#224; jour manuelle imm&eacute;diate de la base Data sur Lcs";
 echo "<INPUT type='hidden' name='rsync' value='1'>";
 echo "&nbsp;<input type=\"submit\" value=\"--> GO\">\n";
 echo "</form>";
 echo "<br>";
 
 echo "<form method=\"get\" action=\"majbaz.php\">";
-echo "<H2>Mise a jour automatique</H2>";
-echo "Heure de mise a jour quotidienne ";
+echo "<H2>Mise &#224; jour automatique</H2>";
+echo "Heure de mise &#224; jour ";
 echo "<INPUT type='hidden' name='cron' value='1'>";
 echo "<select size='1' name='hour'>";
+echo "<OPTION VALUE='*'>*</OPTION>";
 for ($i=0;$i<24;$i++) {
  if (strcmp("$i","$set_hour") == 0) {
  echo "<OPTION VALUE=$i SELECTED>$i</OPTION>";
@@ -45,7 +46,6 @@ for ($i=0;$i<24;$i++) {
  echo "<OPTION VALUE=$i>$i</OPTION>";
  }
 
-echo "<OPTION VALUE=$i>$i</OPTION>";
 }
 echo "</select> h ";
 echo "<select size='1' name='min'>";
@@ -60,8 +60,18 @@ for ($i=0;$i<60;$i+=5) {
 echo "</select> mn ";
 echo "&nbsp;<input type=\"submit\" value=\"Enregistrer\">\n";
 echo "</form>";
+echo "(R&#233;gler l'heure sur * pour faire mise &#224; jour toutes les heures)";
 echo "<br><br>";
 
+$filename = "synchro.log"; 
+if (file_exists($filename)) {
+	echo "<H2>Compte-rendu de la mise &#224; jour</H2>";
+	$handle = fopen ($filename, "r"); 
+	$contenu = fread ($handle, filesize ($filename));
+	fclose ($handle);
+	echo utf8_decode($contenu);
+	}
+echo "<br><br>";
 
 require ("pieds_de_page.php");
 }
