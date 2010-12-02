9d65e5b6d750ffdca6f7517464e8b95310d08f8a
jlcf <jlcf@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2010-12-02 13:23:04 +0000

    lcs-spip / branche 2.4.8 : Fix rights o,n tmp and IMG in chown.sh
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/LCS/LCS2.0ndg/sources/branches/2.4.8@3664 1012e355-7652-49d3-8fd6-1f9b0b464cea

--- lcs-spip/conf/Scripts/chown.sh
+++ lcs-spip/conf/Scripts/chown.sh
@@ -2,21 +2,23 @@
 
 chown -R root:root /usr/share/doc/lcs/$1
 chown -R root:root /var/lib/lcs/$1
+
 chown -R www-data:www-data /usr/share/lcs/$1
 chown root:lcs-users /usr/share/lcs/$1
-
 chmod 750 /usr/share/lcs/$1
+
 if [ -d /usr/share/lcs/$1/CACHE/ ]; then
        chmod g+w /usr/share/lcs/$1/CACHE/
 fi
-chmod g+w /usr/share/lcs/$1/IMG/
+
 if [ -d /usr/share/lcs/$1/ecrire/data ]; then
     chmod g+w /usr/share/lcs/$1/ecrire/data
     chown spip.manager /usr/share/lcs/$1/ecrire/data
 fi
-chmod g+w /usr/share/lcs/$1/ecrire/
+
 
 chown spip.manager /usr/share/lcs/$1/config/mes_options.php
+chown -R spip.manager /usr/share/lcs/$1/IMG
 
 # Mise en place d'un repertoire plugins
 chown -R www-data:www-data /usr/share/lcs/$1/plugins
@@ -30,4 +32,3 @@ echo "Positionnement des droits sur les nouveaux repertoires tmp et local "
 chown -R spip.manager:www-data /usr/share/lcs/$1/tmp
 chmod 770 /usr/share/lcs/$1/tmp
 chown -R www-data:www-data /usr/share/lcs/$1/local
-chown -R www-data:www-data /usr/share/lcs/$1/tmp
