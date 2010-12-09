485550ecad4d52cab2d53d931c1f305cd04a370d
rhertzog <rhertzog@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2010-12-09 13:43:51 +0000

    lcs-zonep: new package
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/LCS/LCS2.0ndg/sources/branches/2.4.8@3687 1012e355-7652-49d3-8fd6-1f9b0b464cea

---
+++ lcs-zonep/usr/share/lcs/lcs-web-hooks.d/post_auth/update_zonep_config.php
@@ -0,0 +1,52 @@
+<?php
+  /*
+   * Update AutoFS configuration file with user's credentials
+   *
+   * @param login string. The login used by the user.
+   *
+   * @param password string. The password of the user.
+   *
+   * @return status bool. True on success, false otherwise
+   */
+function hook_post_auth_update_zonep_config($login = null, $password = null) {
+    global $ad_server, $ad_base_dn, $ad_bind_dn, $ad_bind_pw;
+
+    // Check arguments
+    if(!is_string($login) or !is_string($password))
+	return false;
+
+    // Connect to LDAP
+    $ds = ldap_connect($ad_server);
+    if(!$ds)
+	return false;
+
+    // Bind en admin
+    $r = ldap_bind($ds, $ad_bind_dn, $ad_bind_pw);
+    if(!$r)
+	return false;
+
+    // Fetch UNC from Active Directory
+    $attributes = array('homeDirectory');
+
+    $sr = ldap_search($ds, $ad_base_dn, "(sAMAccountName=$login)", $attributes);
+    if (! $sr)
+	return false;
+
+    $entries = ldap_get_entries($ds, $sr);
+
+    if(empty($entries[0]['homedirectory'][0]))
+	return false;
+
+    $smb_share = str_replace('\\', '/', $entries[0]['homedirectory'][0]);
+
+    // Call sudo wrapper to create autofs configuration file
+    $handle = popen('sudo lcs-zonep-update-credentials', 'w');
+    fwrite($handle, "$login\n$password\n$smb_share\n");
+    $status = pclose($handle) >> 8;
+    if ($status != 0)
+	return false;
+
+    return true;
+}
+
+?>
