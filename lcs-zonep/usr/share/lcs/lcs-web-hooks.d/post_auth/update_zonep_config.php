98897f104247936b56a430c8bb0dfdb4c790454e
jlcf <jlcf@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2011-01-16 15:36:10 +0000

    lcs-zonep / br 2.4.8 : Modify update_zonep_config.php for SE3 hoem access
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/LCS/LCS2.0ndg/sources/branches/2.4.8@3735 1012e355-7652-49d3-8fd6-1f9b0b464cea

--- lcs-zonep/usr/share/lcs/lcs-web-hooks.d/post_auth/update_zonep_config.php
+++ lcs-zonep/usr/share/lcs/lcs-web-hooks.d/post_auth/update_zonep_config.php
@@ -10,29 +10,42 @@
    */
 function hook_post_auth_update_zonep_config($login = null, $password = null) {
     global $ad_server, $ad_base_dn, $ad_bind_dn, $ad_bind_pw;
+    global $ldap_server, $ldap_base_dn, $adminRdn, $adminPw, $se3Ip;
 
     // Check arguments
     if(!is_string($login) or !is_string($password))
 	return false;
 
-    // Ensure we have an ActiveDirectory server to contact
-    if (empty($ad_server))
+    // Ensure we have an ActiveDirectory server or LDAP server to contact
+    if (empty($ad_server) && empty($ldap_server))
 	return false;
+	
+	// Connect to AD or LDAP
+	if (!empty($ad_server))    
+		$ds = ldap_connect($ad_server);
+	else
+		$ds = ldap_connect($ldap_server);	
 
-    // Connect to LDAP
-    $ds = ldap_connect($ad_server);
     if(!$ds)
 	return false;
+	
+    // admin Bind on AD or LDAP
+	if (!empty($ad_server))    
+    	$r = ldap_bind($ds, $ad_bind_dn, $ad_bind_pw);
+    else
+    	$r = ldap_bind($ds, $adminRddn.$ldap_base_dn, $adminPw);    
 
-    // Bind en admin
-    $r = ldap_bind($ds, $ad_bind_dn, $ad_bind_pw);
     if(!$r)
 	return false;
 
     // Fetch UNC from Active Directory
     $attributes = array('homeDirectory');
 
-    $sr = ldap_search($ds, $ad_base_dn, "(sAMAccountName=$login)", $attributes);
+	if (!empty($ad_server))
+    	$sr = ldap_search($ds, $ad_base_dn, "(sAMAccountName=$login)", $attributes);
+    else 	
+    	$sr = ldap_search($ds, $ldap_base_dn, "(uid=$login)", $attributes);
+
     if (! $sr)
 	return false;
 
@@ -41,7 +54,10 @@ function hook_post_auth_update_zonep_config($login = null, $password = null) {
     if(empty($entries[0]['homedirectory'][0]))
 	return false;
 
-    $smb_share = str_replace('\\', '/', $entries[0]['homedirectory'][0]);
+    if (!empty($ad_server))
+    	$smb_share = str_replace('\\', '/', $entries[0]['homedirectory'][0]);
+    else
+    	$smb_share = "//$se3Ip/$login";
 
     // Call sudo wrapper to create autofs configuration file
     $handle = popen('sudo lcs-zonep-update-credentials', 'w');
