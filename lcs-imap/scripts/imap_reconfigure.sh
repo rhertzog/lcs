10eb11cce1979c39c4fd3a313e57710b4441aa8d
jlcf <jlcf@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2011-06-24 11:21:43 +0000

    lcs-imap / trunk : Make a 2;4.9 release eXPerimental   * Modify pacakage for ENT authentication base on pam_cas     courier/authdaemonrc.ent courier/aithdaemonrc.lcs     pam.d/imap.ent pam.d/imap.lcs     scripts/imap_reconfigure.sh
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/LCS/LCS2.0ndg/sources/trunk@4011 1012e355-7652-49d3-8fd6-1f9b0b464cea

--- lcs-imap/scripts/imap_reconfigure.sh
+++ lcs-imap/scripts/imap_reconfigure.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Configure courrier imap sur LCS
-# jLCF <(°_-)/> on 15 Mai 2008
+# Configure courrier imap for LCS
+# Jean-Luc Chretien on 24 Juin 2011
 #
 
 if [ "$4" = "" ];
@@ -14,7 +14,22 @@ LDAP_BASE_DN="$2"
 LDAP_ADMIN_RDN="$3"
 LDAP_ADMIN_PW="$4"
 
-sed -i.lcssav 's/authmodulelist="authpam"/authmodulelist="authldap"/g' /etc/courier/authdaemonrc 
+MODAUTH=`mysql -se "select value from lcs_db.params where name='auth_mod';"`
+
+if [ $MODAUTH == "ENT" ];
+then
+	# Configure authdaemonrc for pam_cas
+	cp /etc/lcs/courier/authdaemonrc.ent /etc/courier/authdaemonrc
+	# Configure pam.d/imap
+	cp /etc/lcs/pam.d/imap.ent /etc/pam.d/imap
+else
+	# Configure authdaemonrc for pamldap
+	cp /etc/lcs/courier/authdaemonrc.lcs /etc/courier/authdaemonrc
+	# Configure pam.d/imap
+	cp /etc/lcs/pam.d/imap.lcs /etc/pam.d/imap
+fi
+
+# Configure authldaprc
 sed -e "s/#LDAPSERVER#/$LDAP_SERVER/g" \
     -e "s/#BASEDN#/$LDAP_BASE_DN/g" \
     -e "s/#ADMINRDN#/$LDAP_ADMIN_RDN/g" \
