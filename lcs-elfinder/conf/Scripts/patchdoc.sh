687b410702643932e781cf41a58a8f80511fefb3
jlcf <jlcf@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2011-09-16 09:01:43 +0000

    lcs-elfinder : Move lcs-elfinder from b248 to trunk
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/LCS/LCS2.0ndg/sources/trunk@4046 1012e355-7652-49d3-8fd6-1f9b0b464cea

---
+++ lcs-elfinder/conf/Scripts/patchdoc.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+function get_lcsdb_params() {
+  PARAMS=`echo  "SELECT value FROM params WHERE name='$1'"| mysql lcs_db -N`
+  echo "$PARAMS"
+}
+
+VLANSC=$(get_lcsdb_params vlansc)
+VLANADMINISTRATIF=$(get_lcsdb_params vlanadmin)
+VLANPEDA1=$(get_lcsdb_params vlanpeda1)
+VLANPEDA2=$(get_lcsdb_params vlanpeda2)
+VLANPEDA3=$(get_lcsdb_params vlanpeda3)
+VLANPEDA4=$(get_lcsdb_params vlanpeda4)
+
+LANS="$VLANSC $VLANADMINISTRATIF $VLANPEDA1 $VLANPEDA2 $VLANPEDA3 $VLANPEDA4"
+
+sed -i "s|@IPLAN@|$LANS|g" /usr/share/doc/lcs/elfinder/html/index.html
+
+exit 0
