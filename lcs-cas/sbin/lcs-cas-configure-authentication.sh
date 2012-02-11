3ae369f95086f7b371bd07a149e3c5f4e43719be
jlcf <jlcf@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2012-02-11 13:25:31 +0000

    lcs-cas / trunk : Fix bug in lcs-cas-configure-authentication.sh
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/LCS/LCS2.0ndg/sources/trunk@4299 1012e355-7652-49d3-8fd6-1f9b0b464cea

--- lcs-cas/sbin/lcs-cas-configure-authentication.sh
+++ lcs-cas/sbin/lcs-cas-configure-authentication.sh
@@ -2,7 +2,10 @@
 #!/bin/bash
 # lcs-cas-configure-authentication.sh
 # Configure rubycas-server authentication ldap or mysql
-# 
+#
+USERHOME="/var/lib/lcs/cas"
+CONF="/etc/rubycas-server"
+
 # Get LCS params in lcs_db
 get_lcsdb_params() {
     PARAMS=`echo  "SELECT value FROM params WHERE name='$1'"| mysql lcs_db -N`
@@ -19,8 +22,13 @@ if [ ! -z $AUTH_MOD ]; then
 else
 	MODAUTHENTICATION="ldap"
 fi
+echo "MODAUTH : $MODAUTHENTICATION"
+LCSMGRPASS=`cat /var/www/lcs/includes/config.inc.php | grep "PASSAUTH=" | cut -d '"' -f 2`
+LDAP_SERVER=$(get_lcsdb_params ldap_server)
+LDAP_BASE_DN=$(get_lcsdb_params ldap_base_dn)
 # 
 if [ $MODAUTHENTICATION == "ldap" ]; then
+ 	echo "ldap config"
 	# Ldap authentication
 	cp $USERHOME/config.yml.ldap.in $CONF/config.yml
 	# LCSMGR PASS
@@ -30,8 +38,11 @@ if [ $MODAUTHENTICATION == "ldap" ]; then
 	#LDAP_BASE_DN
 	sed -i s/#LDAP_BASE_DN#/$LDAP_BASE_DN/g $CONF/config.yml
 else
+    echo "mysql config"
 	# Mysql authentication
 	cp $USERHOME/config.yml.mysql.in $CONF/config.yml
 	# LCSMGR PASS
 	sed -i s/#LCSPASS#/$LCSMGRPASS/g $CONF/config.yml
 fi
+# restart CAS service
+invoke-rc.d rubycas-lcs restart
