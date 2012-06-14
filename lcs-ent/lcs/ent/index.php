df13ca8702054425ecba5df3a830f76f466205e8
jlcf <jlcf@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2012-06-14 12:33:57 +0000

    lcs-ent : Add lcs/ent folder
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/LCS/LCS2.0ndg/sources/trunk@4408 1012e355-7652-49d3-8fd6-1f9b0b464cea

---
+++ lcs-ent/lcs/ent/index.php
@@ -0,0 +1,8 @@
+<?php
+	include "/var/www/lcs/includes/headerauth.inc.php";
+?>
+<html>
+	<head>
+		<meta HTTP-EQUIV="Refresh" CONTENT="0;URL=<?php echo "http://".$ent_hostname ?>">
+	</head>
+</html>
