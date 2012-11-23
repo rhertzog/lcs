06b0d90b7ae79e534c4a4ba45db1ece7ddb1d545
jlcf <jlcf@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2012-11-23 16:14:42 +0000

    lcs-security / trunk : Fixes some paths, modify mod_security.php, add documentation...
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/LCS/LCS2.0ndg/sources/trunk@4564 1012e355-7652-49d3-8fd6-1f9b0b464cea

--- lcs-security/var/www/Admin/mod_security.php
+++ lcs-security/var/www/Admin/mod_security.php
@@ -1,17 +1,38 @@
 <?php
 
-//Projet LCS page de parmetre des modules.
+//Projet LCS activation/desactivation du mode securite d'apache.
 
 include ("/var/www/lcs/includes/headerauth.inc.php");
 include ("/var/www/Annu/includes/ldap.inc.php");
 include ("/var/www/Annu/includes/ihm.inc.php");
-header_html();
-$msgIntro = "<H1>Gestion mode S&eacutecurit&eacute</H1>\n";
+
+$reload=$_POST['reload'] ;
+
 list ($idpers, $login)= isauth();
+
+$html = "
+	  <head>\n
+	  	<title>...::: LCS sécurité  :::...</title>\n
+	  	<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />\n";
+echo $html;	  	
+
+if (  isset($reload) ) {
+	echo "<meta HTTP-EQUIV=\"Refresh\" CONTENT=\"1;url='$PHP_SELF'\">\n";	
+} elseif ( file_exists("/tmp/disablemodesecurity.lock") || file_exists("/tmp/enablemodesecurity.lock") )
+	echo "<meta HTTP-EQUIV=\"Refresh\" CONTENT=\"10;url='$PHP_SELF'\">\n";
+
+$html = "	  	
+	    <link  href='../Annu/style.css' rel='StyleSheet' type='text/css'>\n
+	  </head>\n
+	  <body>\n";
+$html .= "<div id='container'>\n<h1>Sécurité serveur web LCS</h1>\n";
+echo $html;
+
 function mktable($title, $content) {
         echo "<h3>$title</h3>\n";
         echo $content;
 }
+
 if (ldap_get_right("lcs_is_admin",$login)!="Y")
   die (gettext("Vous n'avez pas les droits suffisants pour acc&egrave;der &agrave; cette fonction.")."</BODY></HTML>");
 
@@ -22,32 +43,47 @@ function module_enabled($name) {
  
 
 if (module_enabled('mod_security2')) {
-    echo "Le module de s&eacutecurit&eacute est actif.";
-	if (!isset($_POST['disable']))
-	{
-	echo "<form method=post>";
-	 echo "</select></br><input name=disable type='submit' value='Désactiver' /> ";
-	echo "</form>";
-	}
-	else {
-	echo "Le mode s&eacute;curit&eacute; est en cours de d&eacute;sactivation. </br>
-	le serveur Web va &ecirc;tre relanc&eacute";
-	exec("sudo /usr/share/lcs/sbin/lcs-disable-apache-security ");
-	}
+    echo "<h2>Le module de s&eacutecurit&eacute est actif.</h2>";
+    if ( file_exists("/tmp/disablemodesecurity.lock") ) {
+    	#echo "D&eacute;sactivation du mode s&eacute;curit&eacute; en cours !..<br />";
+        echo "<div align='center'>\n
+				<img src=\"Images/wait.gif\" title=\"Patientez...\" align=\"middle\" border=\"0\" ALT=\"Patientez\">\n&nbsp;D&eacute;sactivation du mode s&eacute;curit&eacute; en cours ! Veuillez patienter...
+        	</div>\n";
+    } else {    	
+		if (!isset($_POST['disable']))
+		{
+			echo "<form name ='security' action='mod_security.php' method='post'>\n";
+			echo "	</select></br><input name=disable type='submit' value='Désactiver le mode sécurité' />\n";
+			echo "<input type=\"hidden\" name=\"reload\" value=\"true\">\n";
+			echo "</form>\n";
+		}
+		else {
+			echo "Le mode s&eacute;curit&eacute; est en cours de d&eacute;sactivation. </br>
+			  Le serveur Web va &ecirc;tre relanc&eacute...<br />";
+			exec("sudo /usr/sbin/lcs-disable-apache-security ");
+		}
+    }
 
 } else {
-    echo "L'environnement actuel ne dispose pas du module de s&eacute;curit&eacute;.";
- "Le module de s&eacutecurit&eacute est actif.";
-        if (!isset($_POST['enable']))
-        {
-	echo "<form method=post>";
-         echo "</select></br><input name=enable type='submit' value='Activer' /> ";
-         echo "</form>";
-        }
-        else {
-	        exec("sudo /usr/share/lcs/sbin/lcs-enable-apache-security ");
+    echo "<h2>L'environnement actuel ne dispose pas du module de s&eacute;curit&eacute;.</h2>";
+        if ( file_exists("/tmp/enablemodesecurity.lock") ) {
+        	echo "<div align='center'>
+        			<img src=\"Images/wait.gif\" title=\"Patientez...\" align=\"middle\" border=\"0\" ALT=\"Patientez\">&nbsp;Activation du mode s&eacute;curit&eacute; en cours ! Veuillez patienter...
+        		</div>";
+        } else {	       
+        	if (!isset($_POST['enable']))
+        	{
+				echo "<form name ='security' action='mod_security.php' method='post'>\n";
+         		echo "	</select></br><input name=enable type='submit' value='Activer le mode sécurité' />\n";
+         		echo "<input type=\"hidden\" name=\"reload\" value=\"true\">\n";
+         		echo "</form>\n";
+        	}
+        	else {
+	        	exec("sudo /usr/sbin/lcs-enable-apache-security ");
+        	}
         }
 }
-
+echo "<a href='../doc/security/html/'>Documentation module LCS sécurité</a>\n";
+echo "</div><!-- Fin container-->\n";
 include ("/var/www/lcs/includes/pieds_de_page.inc.php");
 ?>
