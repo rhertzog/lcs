8ea69e305a8788c27d5c0783d7bd87e595025683
misterphi <misterphi@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2012-12-13 10:53:45 +0000

    lcs-agendas : delete -moz in lcs.css
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/LCS/LCS2.0ndg/sources/trunk@4584 1012e355-7652-49d3-8fd6-1f9b0b464cea

--- lcs-agendas/Agendas/includes/lcs.css.php
+++ lcs-agendas/Agendas/includes/lcs.css.php
@@ -3,78 +3,78 @@
 echo '
 .main, .main th, .main th.weekend {
 	padding-top :0px;
-	-moz-border-radius:10px;
-    -webkit-border-radius:10px;
-    -moz-box-shadow: -2px -3px 4px rgba(0,0,0,0.3);
-    -webkit-box-shadow: -2px -3px 4px rgba(0,0,0,0.3);
-    opacity:0.7;  
+	border-radius:10px;
+    	-webkit-border-radius:10px;
+    	box-shadow: -2px -3px 4px rgba(0,0,0,0.3);
+    	-webkit-box-shadow: -2px -3px 4px rgba(0,0,0,0.3);
+   	opacity:0.7;  
   }
   
 .main td  {
     vertical-align:top;
-	-moz-border-radius-bottomleft:10px;
-	-moz-border-radius-bottomright:10px;
-	-moz-border-radius-topleft:10px;
-	-moz-border-radius-topright:10px;
-	-moz-border-radius:10px;
-	-moz-box-shadow: 0px 20px 10px -10px rgba(255,255,255,0.3) inset;
+	border-radius-bottomleft:10px;
+	border-radius-bottomright:10px;
+	border-radius-topleft:10px;
+	border-radius-topright:10px;
+	border-radius:10px;
+	box-shadow: 0px 20px 10px -10px rgba(255,255,255,0.3) inset;
 	-webkit-box-shadow: inset 0px 20px 10px -10px rgba(255,255,255,0.3) ;
-    -webkit-border-radius:10px; 
+ 	-webkit-border-radius:10px; 
 	font-weight:normal;
 	width:100px;
   }
 
 #week .main th a:hover {
-    color:rgb(252,225,11);
+	color:rgb(252,225,11);
   }
 
 #week .main th.today { 
- opacity:1;
+	opacity:1;
  }
  
 .main td.hasevents {
- -moz-box-shadow: 0px -20px 10px -10px rgba(0,0,0,0.3) inset;
- -webkit-box-shadow:  inset 0px -20px 10px -10px rgba(0,0,0,0.3);
+ 	box-shadow: 0px -20px 10px -10px rgba(0,0,0,0.3) inset;
+ 	-webkit-box-shadow:  inset 0px -20px 10px -10px rgba(0,0,0,0.3);
  }
  
 .main th.row{
-  font-size:';
-  echo intval(-$GLOBALS["TIME_SLOTS"]*0.049 + 15.2); 
-  echo 'px;
-  -moz-box-shadow: 0px 20px 10px -10px rgba(255,255,255,0.3) inset;
-  -webkit-box-shadow: inset 0px 20px 10px -10px rgba(255,255,255,0.3) ;
+  	font-size:';
+  	echo intval(-$GLOBALS["TIME_SLOTS"]*0.049 + 15.2); 
+  	echo 'px;
+  	box-shadow: 0px 20px 10px -10px rgba(255,255,255,0.3) inset;
+  	-webkit-box-shadow: inset 0px 20px 10px -10px rgba(255,255,255,0.3) ;
  }
 
 a.entry {
-font-weight:bold;
- opacity:1;
+	font-weight:bold;
+ 	opacity:1;
  }
  
 a.layerentry {
- font-style: italic ;
- background:#FFFFFF;
- opacity:1;
+ 	font-style: italic ;
+ 	background:#FFFFFF;
+ 	opacity:1;
  }
  
 #week .main th.empty {
- vertical-align:middle;
- color : #000086;
- opacity:2.0;
- background-color: #F8F8FF;
- -moz-box-shadow: -2px -3px 4px #000;
- -webkit-box-shadow:  -2px -3px 4px #000;
+ 	vertical-align:middle;
+ 	color : #000086;
+ 	opacity:2.0;
+ 	background-color: #F8F8FF;
+ 	box-shadow: -2px -3px 4px #000;
+ 	-webkit-box-shadow:  -2px -3px 4px #000;
  }
 
 #day .title .date, .title .date {
-   font-size:18px;
+   	font-size:18px;
  }
 
 #week .main {
-    border-collapse:separate;
+    	border-collapse:separate;
  }
 
 table {
-    border-spacing:1px;
+    	border-spacing:1px;
  }
  
 #week .main th.row, #day .glance td, .glance th.row {
