a8c68fe71c48d79783e6957815adae698fe97823
jlcf <jlcf@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2013-02-01 17:39:09 +0000

    lcs-certmanager / trunk : Fix bug in gencertssl.php
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/LCS/LCS2.0ndg/sources/trunk@4635 1012e355-7652-49d3-8fd6-1f9b0b464cea

--- lcs-certmanager/www/Admin/gencertssl.php
+++ lcs-certmanager/www/Admin/gencertssl.php
@@ -12,22 +12,22 @@ $newcert = $_GET['newcert'];
 function msgaide($msg) {
     return ("&nbsp;<u onmouseover=\"this.T_SHADOWWIDTH=5;this.T_STICKY=1;return escape".gettext("('".$msg."')")."\"><img name=\"action_image2\"  src=\"../images/help-info.gif\"></u>");
 }
-$msg1="Régénération du certificat SSL pour les services LCS (CAS, apache-ssl, imap-ssl) une période de 365 jours.";
+$msg1="RÃ©gÃ©nÃ©ration du certificat SSL pour les services LCS (CAS, apache-ssl, imap-ssl) une pÃ©riode de 365 jours.";
 
 if ($idpers == "0") header("Location:$urlauth");
 $html = "
 	  <head>\n
-	  <title>...::: Génération certificat apache ssl  :::...</title>\n
+	  <title>...::: GÃ©nÃ©ration certificat apache ssl  :::...</title>\n
 	  <link  href='../Annu/style.css' rel='StyleSheet' type='text/css'>\n
 	  </head>\n
 	  <body>\n";
 $html .= "<div id='container'><h2>Services intranet LCS</h2>\n";
 echo $html;
 if (is_admin("system_is_admin",$login)=="Y") {
-    $html = "<h3>Regénération certificat SSL LCS</h3>\n";
+    $html = "<h3>RegÃ©nÃ©ration certificat SSL LCS</h3>\n";
     if ( ! isset($newcert) ) {
         $html .= "<ul>\n<li>\n";  
-        $html .= "\t<a href=\"gencertssl.php?newcert=1\">Lancer la régénération du certificat.</a>".msgaide($msg1)."\n";
+        $html .= "\t<a href=\"gencertssl.php?newcert=1\">Lancer la rÃ©gÃ©nÃ©ration du certificat.</a>".msgaide($msg1)."\n";
         $html .= "</ul>\n</li>\n";
         echo $html;
     } else {
@@ -40,15 +40,15 @@ if (is_admin("system_is_admin",$login)=="Y") {
             #$organization
             #$organizationalunit
 
-        exec("/usr/sbin/lcs-certssl-gen '$fqn' '$domain' '$country' '$province' '$locality' '$organization' '$organizationalunit'");
+        exec("/usr/bin/sudo /usr/sbin/lcs-certssl-gen '$fqn' '$domain' '$country' '$province' '$locality' '$organization' '$organizationalunit'");
         $html .= "<ul>\n<li>\n";  
-        $html .= "\tLe certificat SSL du serveur LCS a été regénéré pour une période de 365 jours. \n";
+        $html .= "\tLe certificat SSL du serveur LCS a Ã©tÃ© regÃ©nÃ©rÃ© pour une pÃ©riode de 365 jours. \n";
         $html .= "</ul>\n</li>\n";
         echo $html;        
     }
 
 }// fin is_admin
-else echo "Vous n'avez pas les droits nécessaires pour ordonner cette action...";
+else echo "Vous n'avez pas les droits nÃ©cessaires pour ordonner cette action...";
 echo "</div><!-- Fin container-->\n";
 include ("../lcs/includes/pieds_de_page.inc.php");
 ?>
