3cc4338bbc33a951cb14e09679520712573dafc6
misterphi <misterphi@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2013-05-17 09:47:39 +0000

    lcs-pma : upgrade to version 4.0.1
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/LCS/LCS2.0ndg/sources/trunk@4661 1012e355-7652-49d3-8fd6-1f9b0b464cea

---
+++ lcs-pma/pma/libraries/plugins/auth/auth_lcs.php
@@ -0,0 +1,14 @@
+<?php
+// modif lcs
+ include "/var/www/lcs/includes/headerauth.inc.php";
+ list ($idpers,$login) = isauth();
+ if ($idpers) {
+     $_LCS['pass']= urldecode( xoft_decode($_COOKIE['LCSuser'],$key_priv) );
+     $_LCS['login']=$login;
+ }
+ if ( isset($_LCS['login']) ) {
+        $PHP_AUTH_USER =  $_LCS['login'];
+        $PHP_AUTH_PW = $_LCS['pass'];
+        define ("AUTHLCS",'OK',true); 
+          }
+    ?>
