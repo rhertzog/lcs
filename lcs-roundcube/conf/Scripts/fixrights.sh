0d681718bdd96af6151c3e4fcbb2ed35bbab9daf
jlcf <jlcf@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2013-10-03 09:11:47 +0000

    lcs-roundcube / trunk : Add 2.4.10~6 package in trunk
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/LCS/LCS2.0ndg/sources/trunk@4727 1012e355-7652-49d3-8fd6-1f9b0b464cea

---
+++ lcs-roundcube/conf/Scripts/fixrights.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+chown -R root:root /usr/share/doc/lcs/roundcube
+chown -R root:root /var/lib/lcs/roundcube
+chown -R root:www-data /usr/share/lcs/roundcube
+chmod -R 750 /usr/share/lcs/roundcube
+chmod -R 770 /usr/share/lcs/roundcube/temp
+chmod -R 770 /usr/share/lcs/roundcube/logs
+if [ -d /var/log/lcs/roundcube ]; then
+	chown -R www-data:www-data /var/log/lcs/roundcube
+fi
