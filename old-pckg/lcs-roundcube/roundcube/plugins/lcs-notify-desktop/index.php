fa02dbdfa217667c85c521a541d179ff27effe96
jlcf <jlcf@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2013-10-03 09:08:31 +0000

    old_pckg : cp lcs-roundcube 2.4.9.2
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/LCS/LCS2.0ndg/sources/trunk@4725 1012e355-7652-49d3-8fd6-1f9b0b464cea

---
+++ old-pckg/lcs-roundcube/roundcube/plugins/lcs-notify-desktop/index.php
@@ -0,0 +1,34 @@
+<?php
+#error_reporting(E_ALL);
+#ini_set('display_errors','On');
+
+include ("/var/www/lcs/includes/headerauth.inc.php");
+
+if ( $auth_mod != "ENT" ) {
+	list ($idpers, $login)= isauth();		
+	# Search and decode LCS cookie pass
+	if ($idpers != "0") {
+		$pass = urldecode( xoft_decode($_COOKIE['LCSuser'],$key_priv) );				  				  
+		$mbox = imap_open("{localhost:143/imap/novalidate-cert}INBOX", $login, $pass);
+		$status = imap_status($mbox,"{localhost:143/imap/novalidate-cert}INBOX", SA_UNSEEN);
+
+		if ($status) {
+			// Form message and output HTML
+			if ($status->unseen > 0) {
+  				$msg = '<p STYLE="FONT-SIZE:11pt;FONT-WEIGHT:BOLD;COLOR:#fdb218">Vous avez '.$status->unseen;
+  				if ($status->unseen == 1) 
+    				$msg .= ' nouveau message';
+  				else 
+    				$msg .= ' nouveaux messages';
+				$msg .= "</p>\n";
+			} else 
+  				$msg = "";
+		} else
+			$msg = "<p>Erreur imap : " . imap_last_error() . "</p>";
+		imap_close($mbox);
+	} else {
+		$msg ="DTQ";	
+	}
+}		
+echo $msg;
+?>
