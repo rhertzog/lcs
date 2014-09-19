d142fdb11cf50ab4c071e9ef15bde2c17fccc945
jlcf <jlcf@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2014-09-19 08:35:36 +0000

    lcs-web / trunk : Make a XP 2.4.10~6 release
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/LCS/LCS2.0ndg/sources/trunk@5128 1012e355-7652-49d3-8fd6-1f9b0b464cea

---
+++ lcs-web/lcs/Plugins/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0//EN">
+<html>
+<head>
+  <title>...::: Serveur LCS :::...</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <LINK  href='../style.css' rel='StyleSheet' type='text/css'>
+</head>
+<body>
+<br><br><br><br><br>
+<div align="center"><h3>Vous n'avez pas la permission d'acc&#233;der au contenu de ce r&#233;pertoire !</h1></div>
+<br><br><br><br><br>
+</body>
+</html>
+<?
+        include ("/var/www/lcs/includes/config.inc.php");
+        include ("/var/www/lcs/includes/pieds_de_page.inc.php");
+?>
