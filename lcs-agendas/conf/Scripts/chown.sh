1883e3204223d8ea077daecf6512dfd4e0d76b20
misterphi <misterphi@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2014-09-25 14:39:14 +0000

    lcs-agendas_trunk : fix security rules
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/LCS/LCS2.0ndg/sources/trunk@5140 1012e355-7652-49d3-8fd6-1f9b0b464cea

--- lcs-agendas/conf/Scripts/chown.sh
+++ lcs-agendas/conf/Scripts/chown.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 chown -R root:root /usr/share/doc/lcs/$1
 chown -R root:root /var/lib/lcs/$1
-chown -R www-data:www-data /usr/share/lcs/Plugins/$1
-chmod 750 /usr/share/lcs/Plugins/$1
+chown -R root:www-data /usr/share/lcs/Plugins/$1
+chmod -R 750 /usr/share/lcs/Plugins/$1
