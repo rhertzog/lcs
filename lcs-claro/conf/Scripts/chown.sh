5791d0508b1907cec10bd587bafa6db20183d96c
misterphi <misterphi@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2014-09-26 08:31:48 +0000

    lcs-claro_trunk : fix rights on tree
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/LCS/LCS2.0ndg/sources/trunk@5143 1012e355-7652-49d3-8fd6-1f9b0b464cea

--- lcs-claro/conf/Scripts/chown.sh
+++ lcs-claro/conf/Scripts/chown.sh
@@ -2,5 +2,7 @@
 module=`echo $1 | tr [:upper:] [:lower:]`
 chown -R root:root /usr/share/doc/lcs/$module
 chown -R root:root /var/lib/lcs/$1
-chown -R www-data:www-data /usr/share/lcs/Plugins/$1
-chmod 750 /usr/share/lcs/Plugins/$1
+chown -R root:www-data /usr/share/lcs/Plugins/$1
+chmod 770 /usr/share/lcs/Plugins/$1
+chmod 760 /usr/share/lcs/Plugins/$1/platform/conf/*.php
+chmod -R 770 /usr/share/lcs/Plugins/$1/courses/
