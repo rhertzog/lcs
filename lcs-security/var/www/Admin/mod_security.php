b75e04111baeff0eafc5e30a0e24d516b1a1baa2
jlcf <jlcf@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2014-09-26 08:37:30 +0000

    lcs-security / trunk : make a XP release
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/LCS/LCS2.0ndg/sources/trunk@5144 1012e355-7652-49d3-8fd6-1f9b0b464cea

--- lcs-security/var/www/Admin/mod_security.php
+++ lcs-security/var/www/Admin/mod_security.php
@@ -1,27 +1,42 @@
 <?php
+/*===========================================
+   Projet LcSE3
+   Administration du serveur LCS
+   Equipe Tice academie de Caen
+   Distribue selon les termes de la licence GPL
+   Derniere modification : 04/04/2014
+   ============================================= */
+include "../Annu/includes/check-token.php";
+if (!check_acces()) exit;
 
-//Projet LCS activation/desactivation du mode securite d'apache.
+$login=$_SESSION['login'];
+if (count($_POST)>0) {
+  //configuration objet
+  include ("../lcs/includes/htmlpurifier/library/HTMLPurifier.auto.php");
+  $config = HTMLPurifier_Config::createDefault();
+  $purifier = new HTMLPurifier($config);
+  //purification des variables
+   if (isset($_POST['reload'])) $reload=$purifier->purify($_POST['reload'] );
+  if (isset($_POST['enable'])) $enable = $purifier->purify($_POST['enable'] );
+  if (isset($_POST['disable'])) $disable= $purifier->purify($_POST['disable'] );
+}
 
 include ("/var/www/lcs/includes/headerauth.inc.php");
 include ("/var/www/Annu/includes/ldap.inc.php");
 include ("/var/www/Annu/includes/ihm.inc.php");
 
-$reload=$_POST['reload'] ;
-
-list ($idpers, $login)= isauth();
-
 $html = "
 	  <head>\n
 	  	<title>...::: LCS sécurité  :::...</title>\n
 	  	<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />\n";
-echo $html;	  	
+echo $html;
 
 if (  isset($reload) ) {
-	echo "<meta HTTP-EQUIV=\"Refresh\" CONTENT=\"1;url='$PHP_SELF'\">\n";	
+	echo "<meta HTTP-EQUIV=\"Refresh\" CONTENT=\"1;url='$PHP_SELF'\">\n";
 } elseif ( file_exists("/tmp/disablemodesecurity.lock") || file_exists("/tmp/enablemodesecurity.lock") )
 	echo "<meta HTTP-EQUIV=\"Refresh\" CONTENT=\"10;url='$PHP_SELF'\">\n";
 
-$html = "	  	
+$html = "
 	    <link  href='../Annu/style.css' rel='StyleSheet' type='text/css'>\n
 	  </head>\n
 	  <body>\n";
@@ -40,7 +55,7 @@ if (ldap_get_right("lcs_is_admin",$login)!="Y")
 function module_enabled($name) {
     return function_exists('apache_get_modules') && in_array($name, apache_get_modules());
 }
- 
+
 
 if (module_enabled('mod_security2')) {
     echo "<h2>Le module de s&eacutecurit&eacute est actif.</h2>";
@@ -49,11 +64,12 @@ if (module_enabled('mod_security2')) {
         echo "<div align='center'>\n
 				<img src=\"Images/wait.gif\" title=\"Patientez...\" align=\"middle\" border=\"0\" ALT=\"Patientez\">\n&nbsp;D&eacute;sactivation du mode s&eacute;curit&eacute; en cours ! Veuillez patienter...
         	</div>\n";
-    } else {    	
-		if (!isset($_POST['disable']))
+    } else {
+		if (!isset($disable))
 		{
 			echo "<form name ='security' action='mod_security.php' method='post'>\n";
 			echo "	</select></br><input name=disable type='submit' value='Désactiver le mode sécurité' />\n";
+                                                      echo '<input name="jeton" type="hidden"  value="'.md5($_SESSION['token'].htmlentities($_SERVER['PHP_SELF'])).'" />';
 			echo "<input type=\"hidden\" name=\"reload\" value=\"true\">\n";
 			echo "</form>\n";
 		}
@@ -70,11 +86,12 @@ if (module_enabled('mod_security2')) {
         	echo "<div align='center'>
         			<img src=\"Images/wait.gif\" title=\"Patientez...\" align=\"middle\" border=\"0\" ALT=\"Patientez\">&nbsp;Activation du mode s&eacute;curit&eacute; en cours ! Veuillez patienter...
         		</div>";
-        } else {	       
-        	if (!isset($_POST['enable']))
+        } else {
+        	if (!isset($enable))
         	{
-				echo "<form name ='security' action='mod_security.php' method='post'>\n";
+		echo "<form name ='security' action='mod_security.php' method='post'>\n";
          		echo "	</select></br><input name=enable type='submit' value='Activer le mode sécurité' />\n";
+                                    echo '<input name="jeton" type="hidden"  value="'.md5($_SESSION['token'].htmlentities($_SERVER['PHP_SELF'])).'" />';
          		echo "<input type=\"hidden\" name=\"reload\" value=\"true\">\n";
          		echo "</form>\n";
         	}
