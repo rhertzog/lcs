6cd59976f77be2623fae39c297e2a6924b42ed60
misterphi <misterphi@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2014-09-26 13:44:43 +0000

    lcs-gepi_trunk : fix rights on tree
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/LCS/LCS2.0ndg/sources/trunk@5151 1012e355-7652-49d3-8fd6-1f9b0b464cea

--- lcs-gepi/conf/Scripts/chown.sh
+++ lcs-gepi/conf/Scripts/chown.sh
@@ -2,5 +2,11 @@
 module=`echo $1 | tr [:upper:] [:lower:]`
 chown -R root:root /usr/share/doc/lcs/$module
 chown -R root:root /var/lib/lcs/$1
-chown -R www-data:www-data /usr/share/lcs/Plugins/$1
-chmod 750 /usr/share/lcs/Plugins/$1
+chown -R root:www-data /usr/share/lcs/Plugins/$1
+chmod -R 750 /usr/share/lcs/Plugins/$1
+CHEMIN=`pwd`
+cd  /usr/share/lcs/Plugins/$1
+chmod  770 documents documents/archives images images/background photos backup artichow/cache mod_ooo/tmp lib/standalone/HTMLPurifier/DefinitionCache/Serializer 
+chmod -R 770 temp mod_ooo/mes_modeles 
+cd $CHEMIN 
+
