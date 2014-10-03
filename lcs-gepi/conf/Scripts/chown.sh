d7463214b649e5cb4a021d3c88ff4f8428fd66d7
misterphi <misterphi@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2014-10-03 07:44:59 +0000

    lcs-gepi_trunk : add recursive right w on backup directory
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/LCS/LCS2.0ndg/sources/trunk@5165 1012e355-7652-49d3-8fd6-1f9b0b464cea

--- lcs-gepi/conf/Scripts/chown.sh
+++ lcs-gepi/conf/Scripts/chown.sh
@@ -6,7 +6,7 @@ chown -R root:www-data /usr/share/lcs/Plugins/$1
 chmod -R 750 /usr/share/lcs/Plugins/$1
 CHEMIN=`pwd`
 cd  /usr/share/lcs/Plugins/$1
-chmod  770 documents documents/archives images images/background photos backup artichow/cache mod_ooo/tmp lib/standalone/HTMLPurifier/DefinitionCache/Serializer 
-chmod -R 770 temp mod_ooo/mes_modeles 
+chmod  770 documents documents/archives images images/background photos artichow/cache mod_ooo/tmp lib/standalone/HTMLPurifier/DefinitionCache/Serializer 
+chmod -R 770 temp mod_ooo/mes_modeles backup 
 cd $CHEMIN 
 
