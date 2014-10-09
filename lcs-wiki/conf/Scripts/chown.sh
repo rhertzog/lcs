b146ef16defffb5246f6858832f84ebf49c42847
misterphi <misterphi@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2014-10-09 13:44:02 +0000

    lcs-wiki_trunk : fix rights on tree
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/LCS/LCS2.0ndg/sources/trunk@5169 1012e355-7652-49d3-8fd6-1f9b0b464cea

--- lcs-wiki/conf/Scripts/chown.sh
+++ lcs-wiki/conf/Scripts/chown.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 chown -R root:root /usr/share/doc/lcs/$1
 chown -R root:root /var/lib/lcs/$1
-chown -R www-data:www-data /usr/share/lcs/Plugins/$1
+chown -R root:www-data /usr/share/lcs/Plugins/$1
 chmod 750 /usr/share/lcs/Plugins/$1
+chmod 770 /usr/share/lcs/Plugins/$1/wakka.config.php
