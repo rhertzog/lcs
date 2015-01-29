a51eadac8ebeb059a3e3c21023699eb67b9ed61f
jlcf <jlcf@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2015-01-29 12:51:38 +0000

    remove lcs-clientftp
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/LCS/LCS2.0ndg/sources/trunk@5246 1012e355-7652-49d3-8fd6-1f9b0b464cea

--- lcs-clientftp/conf/Scripts/chown.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-chown -R root:root /usr/share/doc/lcs/$1
-chown -R root:root /var/lib/lcs/$1
-chown -R www-data:www-data /usr/share/lcs/$1
-chmod 750 /usr/share/lcs/$1
-chmod 775 /usr/share/lcs/$1/tpl/default/templates_c
-chmod 775 /usr/share/lcs/$1/tpl/default/cache
