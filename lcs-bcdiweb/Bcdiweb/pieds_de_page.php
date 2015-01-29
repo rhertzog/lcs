7351aabbc5565e5e7ffa6464a185ab631f977190
jlcf <jlcf@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2015-01-29 12:44:19 +0000

    remove lcs-bcdiweb
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/LCS/LCS2.0ndg/sources/trunk@5240 1012e355-7652-49d3-8fd6-1f9b0b464cea

--- lcs-bcdiweb/Bcdiweb/pieds_de_page.php
+++
@@ -1,6 +0,0 @@
-</DIV><!-- Fin du container -->  
-  <DIV class="pdp">
-    Copyleft &copy; 2001 Module LCS <?php echo 'lcs-bcdiweb'; ?>  Version : <?php echo '#VERSION#' ; ?>
-  </DIV>
-</BODY>
-</HTML>
--- lcs-bcdiweb/conf/Scripts/chown.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-module=`echo $1 | tr [:upper:] [:lower:]`
-chown -R root:root /usr/share/doc/lcs/$module
-chown -R root:root /var/lib/lcs/$1
-chown -R www-data:www-data /usr/share/lcs/Plugins/$1
-chmod 750 /usr/share/lcs/Plugins/$1
-chown root:www-data /etc/cron.d/bcdiweb
-chmod 664 /etc/cron.d/bcdiweb
