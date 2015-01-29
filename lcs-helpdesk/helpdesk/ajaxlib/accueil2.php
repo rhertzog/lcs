a34414fc6637172c6c44311f677a4762e29a2e08
jlcf <jlcf@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2015-01-29 12:48:07 +0000

    remove lcs-helpdesk
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/LCS/LCS2.0ndg/sources/trunk@5244 1012e355-7652-49d3-8fd6-1f9b0b464cea

--- lcs-helpdesk/helpdesk/ajaxlib/accueil2.php
+++
@@ -1,8 +0,0 @@
-<?php require_once('../include/common.inc.php');  
-	//si mustRegister alors on charge le form register
-	
-	if ($HD->authenticate())
-		$json = file_get_contents('../templates/accueil.tpl');
-	$json = str_replace('%MSG%',$HD->getMessage(),$json);
-	die($json);
-?>
--- lcs-helpdesk/helpdesk/ajaxlib/error.php
+++
@@ -1,6 +0,0 @@
-<?php require_once('../include/common.inc.php'); ?>
-
-<H1>Erreur d'authentification !</H1>
-<br /><p>Vous recevez cette erreur parceque le jeton d'authentification n'a pas &#233;t&#233; reconnu.</p>
-
-Votre jeton est: <?php echo $jeton ?>
