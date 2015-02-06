e19e5bceaeaf5a05e27212a9c97ed6fc9b00725d
jlcf <jlcf@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2015-02-06 13:58:35 +0000

    remove old_pckg
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/LCS/LCS2.0ndg/sources/trunk@5263 1012e355-7652-49d3-8fd6-1f9b0b464cea

--- old-pckg/lcs-preinstall/conf/lcs_db-table-swekey.sql
+++
@@ -1,9 +0,0 @@
---
-USE `lcs_db`;
--- 
---
-CREATE TABLE IF NOT EXISTS `swekey` (
-`login` varchar(32) NOT NULL,
-`id_swekey` varchar(32) NOT NULL,
-KEY `id_swekey` (`id_swekey`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
