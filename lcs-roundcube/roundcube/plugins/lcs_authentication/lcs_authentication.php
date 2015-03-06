98af2af9fdf26a52889d125be8a4819110da3bbf
jlcf <jlcf@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2015-03-06 14:39:58 +0000

    lcs-roundcube / trunk : remove idpers from lcs-authentication plugin
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/LCS/LCS2.0ndg/sources/trunk@5279 1012e355-7652-49d3-8fd6-1f9b0b464cea

--- lcs-roundcube/roundcube/plugins/lcs_authentication/lcs_authentication.php
+++ lcs-roundcube/roundcube/plugins/lcs_authentication/lcs_authentication.php
@@ -2,49 +2,22 @@
 	
 	class lcs_authentication extends rcube_plugin
 	{
-	  public $task = 'login';
+		public $task = 'login';
 	
-	  function init()
-	  {
-	    $this->add_hook('startup', array($this, 'startup'));	    
-	    $this->add_hook('authenticate', array($this, 'authenticate'));
-	  }
+		function init()
+	  	{
+	    	$this->add_hook('startup', array($this, 'startup'));	    
+	    	$this->add_hook('authenticate', array($this, 'authenticate'));
+	  	}
 
-	  function startup($args)
-	  {
-	    // change action to login
-	    if ( empty($args['action']) && empty($_SESSION['user_id']) )
-		$args['action'] = 'login';
-		return $args;
-	  }
+	  	function startup($args)
+	  	{
+	    	// change action to login
+	    	if ( empty($args['action']) && empty($_SESSION['user_id']) )
+				$args['action'] = 'login';
+			return $args;
+	  	}
 	
-	  function authenticate($args)
-	  {	
-		$IDPERS=0;$LOGIN="";
-		if (! empty($_COOKIE["LCSAuth"]))
-		{ 
-            $SESS=$_COOKIE["LCSAuth"];
-
-			include ("/usr/share/lcs/roundcube/plugins/lcs_authentication/config_auth_lcs.inc.php");
-			# Search idpers
-			$IDPERS=exec ("mysql -e \"SELECT idpers from $DBAUTH.sessions where sess='$SESS'\" -u $USERAUTH -p$PASSAUTH");
-
-			# Search login
-			$LOGIN=exec ("mysql -e \"SELECT login FROM $DBAUTH.personne WHERE id=$IDPERS \" -u $USERAUTH -p$PASSAUTH");
-	
-			# Search and decode LCS cookie pass
-			if ($IDPERS != "0") 
-				  $PASS = urldecode( xoft_decode($_COOKIE['LCSuser'],$key_priv) );	
-
-	    	if ( !empty($LOGIN) && !empty($PASS) ) {
-	      		$args['user'] = $LOGIN;
-	      		$args['pass'] = $PASS;
-	    	}
-	    	$args['cookiecheck'] = false;
-	    	 $args['valid'] = true;
-	    	return $args;
-	    }
-	  }
-	  
-	  
+	  	function authenticate($args) {			$LOGIN="";      	if (! empty($_COOKIE["Lcs"])) {        		require_once ("/var/www/lcs/includes/functions.inc.php");        		# Search login        		$file="/var/lib/php5/sess_".$_COOKIE['Lcs'];        		$ch= mb_split('"',file_get_contents ($file));        		$LOGIN=$ch[1];        		# Search and decode LCS cookie pass        		if ($LOGIN != "")  $PASS = urldecode( xoft_decode($_COOKIE['LCSuser'],$key_priv) );        		if ( !empty($LOGIN) && !empty($PASS) ) {            	$args['user'] = $LOGIN;            	$args['pass'] = $PASS;        		}        		$args['cookiecheck'] = false;        		$args['valid'] = true;        		return $args;        	}
+	  	}		  
 	} # End class lcs_authentication
