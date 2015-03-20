50d7cb7e0cb72b535640eaa50a62a117f22fab04
misterphi <misterphi@1012e355-7652-49d3-8fd6-1f9b0b464cea>
2015-03-20 08:58:24 +0000

    lcs-agendas_trunk : delete function isauth (bis)
    git-svn-id: https://svn.tice.ac-caen.fr/svn/LcsSE3/LCS/LCS2.0ndg/sources/trunk@5291 1012e355-7652-49d3-8fd6-1f9b0b464cea

--- lcs-agendas/Agendas/includes/validate.php
+++ lcs-agendas/Agendas/includes/validate.php
@@ -1,9 +1,11 @@
 <?php
 include "basedir.inc.php";
 include "$BASEDIR/lcs/includes/headerauth.inc.php";
-// recuperation de l'id dans la table session
-list ($idpers, $login)= isauth();
+session_name("Lcs");
+@session_start();
+$login=$_SESSION['login'];
+ session_write_close();
 if ( empty ($login) ) {
-     do_redirect ( "../../lcs/auth.php" );
+     do_redirect ( "../../lcs/logout.php" );
 }
 ?>
